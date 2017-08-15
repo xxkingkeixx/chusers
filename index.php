8b16a56c91e0d2204fa0df33e3858dd51eb7169b
solicited <mitchanimugen@gmail.com>
2017-08-15 19:18:06 +0000

    test

--- index.php
+++ index.php
@@ -1,17 +1,32 @@
 <!DOCTYPE html>
-<html>
+<html lang = "en-us">
 <head>
-
-
-
-
-<title>
-    Shop
-</title>        
+<title>Shop</title>
+<meta charset ='UTF-8'>
+<link rel = 'stylesheet' type = 'text/css' href = 'https://unpkg.com/purecss@1.0.0/build/pure-min.css'/>
+<meta name="description" content="The chatango username warehouse">
+<meta name="keywords" content="chatango,chatangu,shop,buy,purchase,usernames">
+<meta name="author" content="Kei, Aeri">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
 <p> Hello World</p>
 
 
 </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </html>
