98ddf96a4d010c4ccd2e1405514d48abf4246670
solicited <mitchanimugen@gmail.com>
2017-08-15 22:12:26 +0000

    test

--- index.php
+++ index.php
@@ -209,7 +209,7 @@
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 			<script src="assets/js/main.js"></script>
 			
-			<script type="text/javascript>
+			<script>
 			$("#list")
   .each(function(i) {
     if (i != 0) {
