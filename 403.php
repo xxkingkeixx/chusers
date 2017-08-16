ee51e3204d675f7ca28e6d3720535f5f6d7433bc
solicited <mitchanimugen@gmail.com>
2017-08-16 20:56:55 +0000

    revised 404 and added 403

---
+++ 403.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang = 'en-us'>
+
+<head>
+<meta charset = 'UTF-8'>
+<meta name='description' content='404 page'>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="assets/favicon/favicon.ico" type="image/x-icon">
+<link rel="icon" href="assets/favicon/favicon.ico" type="image/x-icon">
+
+
+
+
+</head>
+
+This page is forbidden.
+
+
+
+
