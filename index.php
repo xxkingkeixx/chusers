56e92214a5e621396c0d9ae3f5467b13f3c40f59
solicited <mitchanimugen@gmail.com>
2017-08-16 17:11:04 +0000

    change pics

--- index.php
+++ index.php
@@ -166,7 +166,7 @@
 							<h2>Comments:</h2>
 							
 						</header>
-						<div id="disqus_thread"></div>
+					<div id="disqus_thread"></div>
 <script>
 
 /**
@@ -180,12 +180,13 @@ this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your pag
 */
 (function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
-s.src = 'https://EXAMPLE.disqus.com/embed.js';
+s.src = 'https://cloudstrato.disqus.com/embed.js';
 s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                            
                             
 					</div>
 				</section>
@@ -250,6 +251,6 @@ $(\"#beep\").attr(\"id\", \"beep-0\"); // get first one into naming convention
 
 
 </script> "; ?>
-<script id="dsq-count-scr" src="//EXAMPLE.disqus.com/count.js" async></script>
+<script id="dsq-count-scr" src="//cloudstrato.disqus.com/count.js" async></script>
 	</body>
 </html>
