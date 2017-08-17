a526332bd51418e8c0e8181040adc5f218c2d3e4
solicited <mitchanimugen@gmail.com>
2017-08-17 17:58:48 +0000

    l

--- post.php
+++ post.php
@@ -31,16 +31,7 @@ $conn = new mysqli($server, $username, $password, $db);
     
     
     
-    mysqli_select_db($db,$conn);
-$sql2 = "SELECT * FROM  store";
-
-$mydata = mysqli_query('$sql2,$conn');
-
-while($record = mysqli_fetch_array($mydata)){
-    echo "<br>";
-    echo  $record['user_name'];
-}
-
+ 
 
     
 
