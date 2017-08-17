a7ef5b3c071983df402351c6a49cd881d2fcbbf2
solicited <mitchanimugen@gmail.com>
2017-08-17 17:53:12 +0000

    l

--- post.php
+++ post.php
@@ -16,16 +16,6 @@ $conn = new mysqli($server, $username, $password, $db);
     
     //Insert Username
     
-    if(!$conn)
-    {
-        echo 'not connected';
-    }
-    
-    if(!mysqli_select_db($conn,'heroku_cd6b3866e127c21'))
-    {
-        echo 'database not selected';
-    }
-    
     
     
     $user_name = $_POST['user_name'];
@@ -33,13 +23,7 @@ $conn = new mysqli($server, $username, $password, $db);
     $sql = "INSERT INTO store (user_name) VALUES ('$user_name')";
     
     
-    if(!mysqli_query($conn,$sql))
-    {
-        echo 'not inserted';
-    }
-    else {
-        echo 'inserted';
-    }
+   
     
     
     
@@ -54,7 +38,7 @@ $mydata = mysql_query('$sql2,$conn');
 
 while($record = mysql_fetch_array($mydata)){
     echo "<br>";
-    echo  $record['username'];
+    echo  $record['user_name'];
 }
 
 
