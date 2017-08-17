77f0207f16b0f11ea04af1692d394bd1e1b5f526
solicited <mitchanimugen@gmail.com>
2017-08-17 20:40:44 +0000

    l

--- post.php
+++ post.php
@@ -11,8 +11,11 @@ $conn = new mysqli($server, $username, $password, $db);
 
 
    
-    
-
+  if( !empty( $_POST) ) {
+    print_r($_POST); exit;
+  }   
+  else {
+      
     
     $user_name = $_POST['user_name'];
     
@@ -29,12 +32,9 @@ $conn = new mysqli($server, $username, $password, $db);
 $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
 while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; }
 
-
+}
    
- if( !empty( $_POST) ) {
-    print_r($_POST); exit;
-       
- }
+
     
 
 
