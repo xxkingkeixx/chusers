a3430718c3e7fe4a4e59f63e2773cd030ee48a39
solicited <mitchanimugen@gmail.com>
2017-08-17 16:46:09 +0000

    db

--- post.php
+++ post.php
@@ -1,8 +1,7 @@
 <?php
 //only process the form if POST isn't empty
 
-if( !empty( $_POST) ) {
-    print_r($_POST); exit;
+
     
     //Check Connection
     
@@ -19,19 +18,19 @@ if( !empty( $_POST) ) {
     
     
     
-    mysqli_select_db($db,$conn);
+    mysql_select_db($db,$conn);
 $sql2 = "SELECT * FROM  store";
 
-$mydata = mysqli_query('$sql2,$conn');
+$mydata = mysql_query('$sql2,$conn');
 
-while($record = mysqli_fetch_array($mydata)){
+while($record = mysql_fetch_array($mydata)){
     echo "<br>";
     echo "$record";
 }
 
 
     
-}
+
 
 
 
