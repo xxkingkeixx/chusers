b101c6a383e58c5e6e8d39f3f434fe0178a1ea73
solicited <mitchanimugen@gmail.com>
2017-08-17 21:10:56 +0000

    l

--- post.php
+++ post.php
@@ -19,8 +19,7 @@ while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name'];
 
      exit;
   }   
-  else {
-      
+
      
     
     $user_name = $_POST['user_name'];
@@ -29,7 +28,11 @@ while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name'];
     
      if (mysql_num_rows($query) != 0)
   {
-      
+       mysqli_select_db($db,$conn);
+$sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
+while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; }
+
+     
       exit;
   }
   else
@@ -49,10 +52,10 @@ while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name'];
 $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
 while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; }
 
-header("Location: " . $_SERVER["REQUEST_URI"]);
+
     exit;
 }
-}
+
    
 
     
