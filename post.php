6b612184c4590eaaef868905334928419e403ba7
solicited <mitchanimugen@gmail.com>
2017-08-17 23:38:01 +0000

    l

--- post.php
+++ post.php
@@ -8,32 +8,37 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
+$user_name = $_POST['user_name']; 
 
+
+mysqli_select_db($db,$conn);
 
    
-  if( !empty( $_POST) ) {
-    print_r($_POST); exit;
-  }   
-  else {
-      
-    
-    $user_name = $_POST['user_name'];
-    
-    $sql = "INSERT INTO store (user_name) VALUES ('$user_name')";
-    
+  if(empty($_POST) ) {
+     $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
+    while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; } 
+     die(); 
+
+  }
   
-    mysqli_query($conn,$sql);
-    
-    
-    
-    
-    
-    mysqli_select_db($db,$conn);
-$sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
-while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; }
-
-}
-   
+  else {
+      $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
+        while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; }
+      if(isset($user_name)){$mysqli_get_users = mysqli_query("SELECT * FROM store where user_name='$user_name'");
+      $get_rows = mysqli_affected_rows($conn);
+      if($get_rows >=1){
+          die();
+      }
+      else
+        {
+            $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
+            while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; }
+            $sql = "INSERT INTO store (user_name) VALUES ('$user_name')";
+            mysqli_query($conn,$sql);
+            exit;
+        }   
+        }
+        }
 
     
 
