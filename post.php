f1f402fe99a2b1dc26cd08c112c66ff1798d493d
solicited <mitchanimugen@gmail.com>
2017-08-17 21:06:35 +0000

    l

--- post.php
+++ post.php
@@ -21,8 +21,20 @@ while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name'];
   }   
   else {
       
+     
     
     $user_name = $_POST['user_name'];
+    
+    $query = mysqli_query("SELECT user_name FROM store WHERE user_name='".$user_name."'");
+    
+     if (mysql_num_rows($query) != 0)
+  {
+      
+      exit;
+  }
+  else
+  {
+     
     
     $sql = "INSERT INTO store (user_name) VALUES ('$user_name')";
     
@@ -37,6 +49,9 @@ while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name'];
 $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
 while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; }
 
+header("Location: " . $_SERVER["REQUEST_URI"]);
+    exit;
+}
 }
    
 
