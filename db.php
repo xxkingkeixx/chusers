0ffd7fecdb1499fe1e86b5880d7b6c75e137b275
solicited <mitchanimugen@gmail.com>
2017-08-17 03:20:23 +0000

    db

--- db.php
+++ db.php
@@ -7,4 +7,12 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
+
+// Check connection
+if (mysqli_connect_errno())
+    {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+
 ?>
