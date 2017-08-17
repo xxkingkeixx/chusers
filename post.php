3af2dcbba10968c76e5d4efb5d21eac1fa74fe7d
solicited <mitchanimugen@gmail.com>
2017-08-17 21:28:53 +0000

    l

--- post.php
+++ post.php
@@ -24,7 +24,7 @@ while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name'];
     
     
     
-    $query = mysqli_query("SELECT count(*) FROM store WHERE user_name = '$_POST'");
+    $query = mysqli_query("SELECT count(*) FROM store WHERE user_name = $_POST");
     
      if(mysqli_fetch_row($query)[0])
   {
