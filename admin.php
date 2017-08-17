ef5c5d4385c2cc0a7dd6fbb9e5a0fbdaec21c1bf
solicited <mitchanimugen@gmail.com>
2017-08-17 19:20:25 +0000

    l

--- admin.php
+++ admin.php
@@ -42,7 +42,7 @@ Your browser isn't invited for super fun audio time.
 					<div class="content">
 						<header>
 							<h2>Add Usernames Here</h2>
-							<form method="post" action='admin.php'>
+							<form method="post" action='post.php'>
   <br>
   <input type="text"  name="user_name"><br>
   <input type="submit" value="Insert">
