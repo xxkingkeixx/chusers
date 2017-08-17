5df44087b6afd8da4357f383983fde6dc446e5d2
solicited <mitchanimugen@gmail.com>
2017-08-17 18:11:50 +0000

    l

--- admin.php
+++ admin.php
@@ -1,5 +1,47 @@
 <?php
-require 'post.php';						?>
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+
+    
+   
+    
+    
+    
+    
+    
+    
+    $user_name = $_POST['user_name'];
+    
+    $sql = "INSERT INTO store (user_name) VALUES ('$user_name')";
+    
+    
+   
+    
+    
+    
+    
+    
+    
+  
+
+
+    
+
+
+
+
+
+
+?>
+
 
 
 <!DOCTYPE html>
