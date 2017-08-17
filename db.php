a2b403e699e186e99f173f001a2ab54b5e2f8a8f
solicited <mitchanimugen@gmail.com>
2017-08-17 14:03:27 +0000

    db

--- db.php
+++ db.php
@@ -10,20 +10,5 @@ $conn = new mysqli($server, $username, $password, $db);
 
 
 
-// Check connection
-if (mysqli_connect_errno())
-    {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-else 
-{
-   printf("Connected to : %s and the DB name is: %s u: %s p: %s" ,$server,$db,$username,$password);
-}
-
-if ($result = $mysqli->query("SELECT DATABASE()")) {
-    $row = $result->fetch_row();
-    printf("Default database is %s.\n", $row[0]);
-    $result->close();
-}
 
 ?>
