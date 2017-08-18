7428512d30a10c87e3d7be22befb0caea9c2a901
solicited <mitchanimugen@gmail.com>
2017-08-18 14:08:41 +0000

    change pics

--- post.php
+++ post.php
@@ -32,7 +32,7 @@ mysqli_select_db($db,$conn);
         {
   $exploded = preg_split('@,@', $user_name, NULL, PREG_SPLIT_NO_EMPTY);
   foreach ($exploded as $value) {
-    echo $value . '<br>';
+    mysqli_query('INSERT INTO store(user_name) VALUES ($value)');
   }
         }
 //$Query = 'INSERT INTO store(user_name) VALUES ($input)';
