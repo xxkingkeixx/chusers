f11d2bfdc42c53c133edd0064ea595e12eee7e62
solicited <mitchanimugen@gmail.com>
2017-08-18 01:29:35 +0000

    change pics

--- admin.php
+++ admin.php
@@ -42,7 +42,7 @@ Your browser isn't invited for super fun audio time.
 					<div class="content">
 						<header>
 							<h2>Add Usernames Here</h2>
-							<form method="post">
+							<form method="post" action-''>
   <br>
   <input type="text"  name="user_name"><br>
   <input type="submit" value="Insert">
