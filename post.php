d996af8f8ca3945b04354bf948da8c3a08078273
solicited <mitchanimugen@gmail.com>
2017-08-18 01:28:24 +0000

    change pics

--- post.php
+++ post.php
@@ -8,26 +8,28 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
+
+ $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
+    while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; } 
+
+
 $user_name = $_POST['user_name']; 
 
 
 mysqli_select_db($db,$conn);
 
- $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
-    while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; } 
+
 
    
   if(empty($_POST) ) {
-     $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
-    while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; } 
+    
      die(); 
 
   }
   
       else
         {
-            $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
-    while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; }
+           
             $sql = "INSERT INTO store (user_name) VALUES ('$user_name')";
             mysqli_query($conn,$sql);
             
