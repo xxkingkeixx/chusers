41bf0d6d3c84f9350261511114edba53ed51b18c
solicited <mitchanimugen@gmail.com>
2017-08-18 02:38:14 +0000

    change pics

--- post.php
+++ post.php
@@ -31,11 +31,11 @@ mysqli_select_db($db,$conn);
         {
            
            
-$myArray = implode(',', $_POST['user_name']);
-
+$myArray = explode(',', '$user_name');
+foreach($myArray as $my_Array){
     $sql="INSERT INTO store (user_name) VALUES {$sql};";
     mysqli_query($conn,$sql);
-
+}
             
    
 
