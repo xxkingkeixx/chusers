133aa8fc3583e54a3ba421d4b44d293d165e6ebc
solicited <mitchanimugen@gmail.com>
2017-08-18 01:58:30 +0000

    change pics

--- post.php
+++ post.php
@@ -31,22 +31,23 @@ mysqli_select_db($db,$conn);
         {
            
             $array = explode(',', $_POST['user_name']);
-             $id=mysqli_insert_id($conn);//get your project id here
+            
     $sql="";    
     foreach($array as $tag_name){
        //modify below to add $id along with $tag_name
-       $sql.="('{$id}','{$tag_name}'),"; // you need to remove last comma else it will throw mysql error  
+       $sql="('{$tag_name}')"; // you need to remove last comma else it will throw mysql error  
     }
 
     if($sql!=""){
     //rtrim to remove last ',' from string. 
     $sql=rtrim($sql,',');
 
-    $sql="INSERT INTO store (project_id,user_name) VALUES {$sql};";
+    $sql="INSERT INTO store (user_name) VALUES {$sql};";
+    mysqli_query($conn,$sql);
+            header("Refresh:0; url=admin.php");
     
     }
-            mysqli_query($conn,$sql);
-            header("Refresh:0; url=admin.php");
+            
             
         }   
       
