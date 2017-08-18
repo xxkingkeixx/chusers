5c73284249f4dfc4512b209c5548192bc983809f
solicited <mitchanimugen@gmail.com>
2017-08-18 02:42:15 +0000

    change pics

--- post.php
+++ post.php
@@ -30,21 +30,25 @@ mysqli_select_db($db,$conn);
       else
         {
            
-           
-$myArray = implode(',', $user_name);
-foreach($myArray as $my_Array){
+            $array = explode(',', $_POST['user_name']);
+            
+    $sql="";    
+    foreach($array as $user_name){
+       //modify below to add $id along with $tag_name
+       $sql="('{$user_name}'),"; // you need to remove last comma else it will throw mysql error  
+    }
+
+    if($sql!=""){
+    //rtrim to remove last ',' from string. 
+    $sql=rtrim($sql,',');
+
     $sql="INSERT INTO store (user_name) VALUES {$sql};";
     mysqli_query($conn,$sql);
-}
-            
-   
-
     
-    
-        }
+    }
             
            
             
-           
+        }   
       
 ?>
