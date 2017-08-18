010592d64141a234977073ca13afbc53c682d5f0
solicited <mitchanimugen@gmail.com>
2017-08-18 01:20:24 +0000

    change pics

--- post.php
+++ post.php
@@ -10,14 +10,14 @@ $db = substr($url["path"], 1);
 $conn = new mysqli($server, $username, $password, $db);
  
 
-print($server);
+
 
 
 if (isset($_POST['user_name']))
 {
     $user = $_POST['user_name'];
 
-    if (mysqli_num_rows(mysqli_query("SELECT * FROM store WHERE user_name='$user'")))
+    if (mysql_num_rows(mysql_query("SELECT * FROM store WHERE user_name='$user'")))
             echo "Name is taken";
     else echo "Name available";
 }
