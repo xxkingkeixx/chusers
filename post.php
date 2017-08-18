56e95ddd539e1278b9741c87c20cd135b2ef4ba1
solicited <mitchanimugen@gmail.com>
2017-08-18 01:26:03 +0000

    change pics

--- post.php
+++ post.php
@@ -13,6 +13,9 @@ $user_name = $_POST['user_name'];
 
 mysqli_select_db($db,$conn);
 
+ $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
+    while($record = $mydata->fetch_assoc()){ echo "<br>"; echo $record['user_name']; } 
+
    
   if(empty($_POST) ) {
      $sql2 = "SELECT * FROM store"; $mydata = $mydata = mysqli_query($conn,$sql2); 
