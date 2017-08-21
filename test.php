f748f0ca20e310d30201331113a73ea712d8d327
solicited <mitchanimugen@gmail.com>
2017-08-21 21:03:23 +0000

    update

--- test.php
+++ test.php
@@ -1,15 +1,19 @@
 <html>
     <body>
-<?php use Sunra\PhpSimple\HtmlDomParser; ?>
-<?php
+<?php use Sunra\PhpSimple\HtmlDomParser;
+
+
+
+
+
 // Retrieve the DOM from a given URL
-$html = file_get_html('http://www.dictionary.com/browse/banausic');
+$dom = HtmlDomParser::file_get_html('http://www.dictionary.com/browse/banausic');
 
 // Find the DIV tag with an id of "myId"
-foreach($html->find('div.def-list') as $e)
+foreach($dom->find('div.def-list') as $e)
     echo $e->innertext . '<br>';
     // Extract all text from a given cell
-echo $html->find('td[align="center"]', 1)->plaintext.'<br><hr>';
+echo $dom->find('td[align="center"]', 1)->plaintext.'<br><hr>';
 ?>
 
 </body>
