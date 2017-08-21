24bf98333f1dda1b17ccfbaaacbba2af3735db7d
solicited <mitchanimugen@gmail.com>
2017-08-21 21:54:24 +0000

    ok

--- models/shopmodule.php
+++ models/shopmodule.php
@@ -1,9 +1,7 @@
 <?php echo '
 
-  <!-- Work Section -->
         <section class="ptb ptb-sm-80">
             <div class="container">
-                <!-- work Filter -->
                 <div class="row">
                     <ul class="col container-filter categories-filter">
                         <li><a class="categories active" data-filter="*">All</a></li>
@@ -11,88 +9,45 @@
                         <li><a class="categories" data-filter=".premium">premium</a></li>
                         <li><a class="categories" data-filter=".regular">regular</a></li>
                         <li><a class="categories" data-filter=".lowest">lowest</a></li>
-
                     </ul>
                 </div>
-                <!-- End work Filter -->
                 
-         
                 <div class="row container-grid nf-col-5"> <!--how many in a row number-->
                 
-                                    <div class="nf-item legendary spacing">
-                        <div class="item-box">
-                            <a href="portfolio-single7-youtube.html">
+                <!------------------------------------------------------------------ START SHOP MODULE ------------------------------------------------------------------>
+                
+                    <div class="nf-item legendary spacing">
+                        <div class="item-box"><a href="#legendary">
                                 <img alt="1" src="http://i.imgur.com/2nov74U.jpg" class="item-container">
-                                <div class="item-mask">
-                                    <div class="item-caption">
-                                        <h5 class="white">Example</h5>
-                                        <p class="white">legendary</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                                    <div class="item-mask"><div class="item-caption">
+                                        <h5 class="white">Example</h5><p class="white">legendary</p>
+                                    </div></div></a></div></div>
             
                     <div class="nf-item premium spacing">
-                        <div class="item-box">
-                            <a href="portfolio-single1.html">
+                        <div class="item-box"><a href="#premium">
                                 <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
-                                <div class="item-mask">
-                                    <div class="item-caption">
-                                        <h5 class="white">Abut</h5>
-                                        <p class="white">premium</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                                <div class="item-mask"><div class="item-caption">
+                                        <h5 class="white">Abut</h5><p class="white">premium</p>
+                                    </div></div></a></div></div>
                     
-                    
-                                        <div class="nf-item regular spacing">
-                        <div class="item-box">
-                            <a href="portfolio-single4.html">
+                    <div class="nf-item regular spacing">
+                        <div class="item-box"><a href="#regular">
                                 <img alt="1" src="http://i.imgur.com/eDprqAd.jpg" class="item-container">
-                                <div class="item-mask">
-                                    <div class="item-caption">
-                                        <h5 class="white">Example</h5>
-                                        <p class="white">regular</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                                <div class="item-mask"><div class="item-caption">
+                                        <h5 class="white">Example</h5><p class="white">regular</p>
+                                    </div></div></a></div></div>
                 
-
-
                     <div class="nf-item lowest spacing">
-                        <div class="item-box">
-                            <a href="portfolio-single2.html">
+                        <div class="item-box"><a href="#lowest">
                                 <img alt="1" src="http://i.imgur.com/4rxy3eM.jpg" class="item-container">
-                                <div class="item-mask">
-                                    <div class="item-caption">
-                                        <h5 class="white">Example</h5>
-                                        <p class="white">lowest</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    
-                   
-                    
-
+                                <div class="item-mask"><div class="item-caption">
+                                        <h5 class="white">Example</h5><p class="white">lowest</p>
+                                    </div></div></a></div></div>
+                                    
+                <!------------------------------------------------------------------ END SHOP MODULE ------------------------------------------------------------------>
+       
+       
         </div>
         </div>
         </section>
-
-
-
-
-
-
-
-
-
-
 ' ; ?>
