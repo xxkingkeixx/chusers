0954475a961a5ea6eb8d82d96c1e64ba20897c83
solicited <mitchanimugen@gmail.com>
2017-08-21 20:56:41 +0000

    update

--- test.php
+++ test.php
@@ -8,6 +8,8 @@ $html = file_get_html('http://www.dictionary.com/browse/banausic');
 // Find the DIV tag with an id of "myId"
 foreach($html->find('div.def-list') as $e)
     echo $e->innertext . '<br>';
+    // Extract all text from a given cell
+echo $html->find('td[align="center"]', 1)->plaintext.'<br><hr>';
 ?>
 
 </body>
