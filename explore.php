5b974f8a70406374318a79598d76350534734701
solicited <mitchanimugen@gmail.com>
2017-08-21 17:33:13 +0000

    l

--- explore.php
+++ explore.php
@@ -88,87 +88,8 @@
             </div>
         </section>
         <!-- End About Section -->
-
-        <!-- Work Section -->
-        <section class="ptb ptb-sm-80">
-            <div class="container">
-                <!-- work Filter -->
-                <div class="row">
-                    <ul class="col container-filter categories-filter">
-                        <li><a class="categories active" data-filter="*">All</a></li>
-                        <li><a class="categories" data-filter=".premium">premium</a></li>
-                        <li><a class="categories" data-filter=".regular">regular</a></li>
-                        <li><a class="categories" data-filter=".lowest">lowest</a></li>
-                        <li><a class="categories" data-filter=".latest">latest</a></li>
-                    </ul>
-                </div>
-                <!-- End work Filter -->
-                <div class="row container-grid nf-col-3">
-                    
-                    
-                    <?php require 'models/shopmodule.php' ?>
-
-
-                    <div class="nf-item premium spacing">
-                        <div class="item-box">
-                            <a href="portfolio-single1.html">
-                                <img alt="1" src="images/exploreimages/portfolio/1.jpg" class="item-container">
-                                <div class="item-mask">
-                                    <div class="item-caption">
-                                        <h5 class="white">Consequat massa quis</h5>
-                                        <p class="white">premium</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="nf-item lowest spacing">
-                        <div class="item-box">
-                            <a href="portfolio-single2.html">
-                                <img alt="1" src="images/exploreimages/portfolio/2.jpg" class="item-container">
-                                <div class="item-mask">
-                                    <div class="item-caption">
-                                        <h5 class="white">Bookmarksgrove right at</h5>
-                                        <p class="white">lowest</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="nf-item regular spacing">
-                        <div class="item-box">
-                            <a href="portfolio-single4.html">
-                                <img alt="1" src="images/exploreimages/portfolio/4.jpg" class="item-container">
-                                <div class="item-mask">
-                                    <div class="item-caption">
-                                        <h5 class="white">Rethoric question ran over</h5>
-                                        <p class="white">regular</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="nf-item latest spacing">
-                        <div class="item-box">
-                            <a href="portfolio-single7-youtube.html">
-                                <img alt="1" src="images/exploreimages/portfolio/8.jpg" class="item-container">
-                                <div class="item-mask">
-                                    <div class="item-caption">
-                                        <h5 class="white">Subline of her own road</h5>
-                                        <p class="white">latest</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-        </div>
-        </div>
-
-        </section>
+ <?php require 'models/shopmodule.php' ?>
+      
         <!-- End Work Section -->
 
         <div class="spacer-60"></div>
