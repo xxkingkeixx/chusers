f3f28e3f090b98916152f8a2ab815c414679aa11
solicited <mitchanimugen@gmail.com>
2017-08-21 22:01:43 +0000

    ok

--- models/shopmodule.php
+++ models/shopmodule.php
@@ -1,22 +1,8 @@
 <?php echo '
 
-        <section class="ptb ptb-sm-80">
-            <div class="container">
-                <div class="row">
-                    <ul class="col container-filter categories-filter">
-                        <li><a class="categories active" data-filter="*">All</a></li>
-                        <li><a class="categories" data-filter=".legendary">legendary</a></li>
-                        <li><a class="categories" data-filter=".premium">premium</a></li>
-                        <li><a class="categories" data-filter=".regular">regular</a></li>
-                        <li><a class="categories" data-filter=".lowest">lowest</a></li>
-                    </ul>
-                </div>
-                
-                <div class="row container-grid nf-col-5"> <!--how many in a row number-->
-                
-                <!------------------------------------------------------------------ START SHOP MODULE ------------------------------------------------------------------>
-                
-                    <div class="nf-item legendary spacing">
+        <!-- 
+        
+                            <div class="nf-item legendary spacing">
                         <div class="item-box"><a href="#legendary">
                                 <img alt="1" src="http://i.imgur.com/2nov74U.jpg" class="item-container">
                                     <div class="item-mask"><div class="item-caption">
@@ -43,7 +29,97 @@
                                 <div class="item-mask"><div class="item-caption">
                                         <h5 class="white">Example</h5><p class="white">lowest</p>
                                     </div></div></a></div></div>
+        -->
+
+        <section class="ptb ptb-sm-80">
+            <div class="container">
+                <div class="row">
+                    <ul class="col container-filter categories-filter">
+                        <li><a class="categories active" data-filter="*">All</a></li>
+                        <li><a class="categories" data-filter=".legendary">legendary</a></li>
+                        <li><a class="categories" data-filter=".premium">premium</a></li>
+                        <li><a class="categories" data-filter=".regular">regular</a></li>
+                        <li><a class="categories" data-filter=".lowest">lowest</a></li>
+                    </ul>
+                </div>
+                
+                <div class="row container-grid nf-col-5"> <!--how many in a row number-->
+                
+                <!------------------------------------------------------------------ START SHOP MODULE ------------------------------------------------------------------>
+                <!-------------------------------------------------------------------------- A -------------------------------------------------------------------------->
+
+<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Abut</h5><p class="white">premium</p>
+                </div></div></a></div></div>
+
+<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Abut</h5><p class="white">premium</p>
+                </div></div></a></div></div>
+
+<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Abut</h5><p class="white">premium</p>
+                </div></div></a></div></div>
                                     
+<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Abut</h5><p class="white">premium</p>
+                </div></div></a></div></div>
+                                    
+<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Abut</h5><p class="white">premium</p>
+                </div></div></a></div></div>
+                                    
+<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Abut</h5><p class="white">premium</p>
+                </div></div></a></div></div>                                 
+
+<div class="nf-item legendary spacing"><div class="item-box"><a href="#legendary">
+    <img alt="1" src="http://i.imgur.com/2nov74U.jpg" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Example</h5><p class="white">legendary</p>
+                </div></div></a></div></div>
+            
+<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Abut</h5><p class="white">premium</p>
+                </div></div></a></div></div>
+                                    
+<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Abut</h5><p class="white">premium</p>
+                </div></div></a></div></div>
+                
+                <!-------------------------------------------------------------------------- A -------------------------------------------------------------------------->
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
                 <!------------------------------------------------------------------ END SHOP MODULE ------------------------------------------------------------------>
        
        
