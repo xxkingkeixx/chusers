b27b0c1d862d80c738845aab82a5b1856353ba00
solicited <mitchanimugen@gmail.com>
2017-08-21 20:47:38 +0000

    update

--- test.php
+++ test.php
@@ -1,14 +1,14 @@
 <html>
     <body>
 <?php use Sunra\PhpSimple\HtmlDomParser; ?>
-
+<?php
 // Retrieve the DOM from a given URL
 $html = file_get_html('http://www.dictionary.com/browse/banausic');
 
 // Find the DIV tag with an id of "myId"
 foreach($html->find('div.def-list') as $e)
     echo $e->innertext . '<br>';
-
+?>
 
 </body>
 </html>
