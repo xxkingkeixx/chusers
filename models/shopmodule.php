475573c8bfa7593a46b6051a04d03395ebdb3faa
solicited <mitchanimugen@gmail.com>
2017-08-21 22:30:49 +0000

    ok

--- models/shopmodule.php
+++ models/shopmodule.php
@@ -1,36 +1,5 @@
 <?php echo '
 
-        <!-- 
-        
-                            <div class="nf-item legendary spacing">
-                        <div class="item-box"><a href="#legendary">
-                                <img alt="1" src="http://i.imgur.com/2nov74U.jpg" class="item-container">
-                                    <div class="item-mask"><div class="item-caption">
-                                        <h5 class="white">Example</h5><p class="white">legendary</p>
-                                    </div></div></a></div></div>
-            
-                    <div class="nf-item premium spacing">
-                        <div class="item-box"><a href="#premium">
-                                <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
-                                <div class="item-mask"><div class="item-caption">
-                                        <h5 class="white">Abut</h5><p class="white">premium</p>
-                                    </div></div></a></div></div>
-                    
-                    <div class="nf-item regular spacing">
-                        <div class="item-box"><a href="#regular">
-                                <img alt="1" src="http://i.imgur.com/eDprqAd.jpg" class="item-container">
-                                <div class="item-mask"><div class="item-caption">
-                                        <h5 class="white">Example</h5><p class="white">regular</p>
-                                    </div></div></a></div></div>
-                
-                    <div class="nf-item lowest spacing">
-                        <div class="item-box"><a href="#lowest">
-                                <img alt="1" src="http://i.imgur.com/4rxy3eM.jpg" class="item-container">
-                                <div class="item-mask"><div class="item-caption">
-                                        <h5 class="white">Example</h5><p class="white">lowest</p>
-                                    </div></div></a></div></div>
-        -->
-
         <section class="ptb ptb-sm-80">
             <div class="container">
                 <div class="row">
@@ -43,13 +12,37 @@
                         <br>
                         <li><a class="categories" data-filter=".a">a</a></li>
                         <li><a class="categories" data-filter=".b">b</a></li>
+                        <li><a class="categories" data-filter=".c">c</a></li>
+                        <li><a class="categories" data-filter=".d">d</a></li>
+                        <li><a class="categories" data-filter=".e">e</a></li>
+                        <li><a class="categories" data-filter=".f">f</a></li>
+                        <li><a class="categories" data-filter=".g">g</a></li>
+                        <li><a class="categories" data-filter=".h">h</a></li>
+                        <li><a class="categories" data-filter=".i">i</a></li>
+                        <li><a class="categories" data-filter=".j">j</a></li>
+                        <li><a class="categories" data-filter=".k">k</a></li>
+                        <li><a class="categories" data-filter=".l">l</a></li>
+                        <li><a class="categories" data-filter=".m">m</a></li>
+                        <li><a class="categories" data-filter=".n">n</a></li>
+                        <li><a class="categories" data-filter=".o">o</a></li>
+                        <li><a class="categories" data-filter=".p">p</a></li>
+                        <li><a class="categories" data-filter=".q">q</a></li>
+                        <li><a class="categories" data-filter=".r">r</a></li>
+                        <li><a class="categories" data-filter=".s">s</a></li>
+                        <li><a class="categories" data-filter=".t">t</a></li>
+                        <li><a class="categories" data-filter=".u">u</a></li>
+                        <li><a class="categories" data-filter=".v">t</a></li>
+                        <li><a class="categories" data-filter=".w">w</a></li>
+                        <li><a class="categories" data-filter=".x">x</a></li>
+                        <li><a class="categories" data-filter=".y">y</a></li>
+                        <li><a class="categories" data-filter=".z">z</a></li>
                     </ul>
                 </div>
                 
                 <div class="row container-grid nf-col-5"> <!--how many in a row number-->
                 
-                <!------------------------------------------------------------------ START SHOP MODULE ------------------------------------------------------------------>
-                <!-------------------------------------------------------------------------- A -------------------------------------------------------------------------->
+<!------------------------------------------------------------------ START SHOP MODULE ------------------------------------------------------------------>
+<!-------------------------------------------------------------------------- A -------------------------------------------------------------------------->
 
 <div class="nf-item premium a spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
@@ -69,91 +62,91 @@
             <h5 class="white">Accurately - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                                     
-<div class="nf-item premium b spacing"><div class="item-box"><a href="#premium">
+<div class="nf-item premium a spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Acquitting - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                                     
-<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+<div class="nf-item premium a spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Addresses - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                                     
-<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+<div class="nf-item premium a spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Adipose - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>                               
 
-<div class="nf-item legendary spacing"><div class="item-box"><a href="#legendary">
+<div class="nf-item legendary a spacing"><div class="item-box"><a href="#legendary">
     <img alt="1" src="http://i.imgur.com/2nov74U.jpg" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Afford - $20</h5><p class="white">legendary</p>
                 </div></div></a></div></div>
             
-<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+<div class="nf-item premium a spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Aimed - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                                     
-<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+<div class="nf-item premium a spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Albany - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
 
-<div class="nf-item lowest spacing"><div class="item-box"><a href="#lowest">
+<div class="nf-item lowest a spacing"><div class="item-box"><a href="#lowest">
     <img alt="1" src="http://i.imgur.com/4rxy3eM.jpg" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Amatory - $1</h5><p class="white">lowest</p>
                 </div></div></a></div></div>
                 
-<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+<div class="nf-item premium a spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Amazons - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                 
-<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+<div class="nf-item premium a spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Anagram - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                 
-<div class="nf-item lowest spacing"><div class="item-box"><a href="#lowest">
+<div class="nf-item lowest a spacing"><div class="item-box"><a href="#lowest">
     <img alt="1" src="http://i.imgur.com/4rxy3eM.jpg" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Anglian - $1</h5><p class="white">lowest</p>
                 </div></div></a></div></div>
                 
-<div class="nf-item lowest spacing"><div class="item-box"><a href="#lowest">
+<div class="nf-item lowest a spacing"><div class="item-box"><a href="#lowest">
     <img alt="1" src="http://i.imgur.com/4rxy3eM.jpg" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Angora - $1</h5><p class="white">lowest</p>
                 </div></div></a></div></div>
                 
-<div class="nf-item regular spacing"><div class="item-box"><a href="#regular">
+<div class="nf-item regular a spacing"><div class="item-box"><a href="#regular">
     <img alt="1" src="http://i.imgur.com/eDprqAd.jpg" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Anticyclone - $5</h5><p class="white">regular</p>
                 </div></div></a></div></div>
                 
-<div class="nf-item regular spacing"><div class="item-box"><a href="#regular">
+<div class="nf-item regular a spacing"><div class="item-box"><a href="#regular">
     <img alt="1" src="http://i.imgur.com/eDprqAd.jpg" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Armchair - $5</h5><p class="white">regular</p>
                 </div></div></a></div></div> 
                 
-<div class="nf-item regular spacing"><div class="item-box"><a href="#regular">
+<div class="nf-item regular a spacing"><div class="item-box"><a href="#regular">
     <img alt="1" src="http://i.imgur.com/eDprqAd.jpg" class="item-container">
         <div class="item-mask"><div class="item-caption">
             <h5 class="white">Armrest - $5</h5><p class="white">regular</p>
                 </div></div></a></div></div>  
                 
-                <!-------------------------------------------------------------------------- A -------------------------------------------------------------------------->
+<!-------------------------------------------------------------------------- A -------------------------------------------------------------------------->
                 
                 
                 
@@ -171,8 +164,7 @@
                 
                 
                 
-                <!------------------------------------------------------------------ END SHOP MODULE ------------------------------------------------------------------>
-       
+<!------------------------------------------------------------------ END SHOP MODULE ------------------------------------------------------------------>
        
         </div>
         </div>
