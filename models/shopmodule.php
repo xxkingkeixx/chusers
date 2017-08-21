3630f1fb881ce243d90aa2e13ee22722c145fc9c
solicited <mitchanimugen@gmail.com>
2017-08-21 22:11:48 +0000

    ok

--- models/shopmodule.php
+++ models/shopmodule.php
@@ -57,49 +57,61 @@
 <div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
-            <h5 class="white">Abut - $10</h5><p class="white">premium</p>
+            <h5 class="white">Accompanied - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
 
 <div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
-            <h5 class="white">Abut - $10</h5><p class="white">premium</p>
+            <h5 class="white">Accurately - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                                     
 <div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
-            <h5 class="white">Abut - $10</h5><p class="white">premium</p>
+            <h5 class="white">Acquitting - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                                     
 <div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
-            <h5 class="white">Abut</h5><p class="white">premium</p>
+            <h5 class="white">Addresses - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                                     
 <div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
-            <h5 class="white">Abut</h5><p class="white">premium</p>
-                </div></div></a></div></div>                                 
+            <h5 class="white">Adipose - $10</h5><p class="white">premium</p>
+                </div></div></a></div></div>                               
 
 <div class="nf-item legendary spacing"><div class="item-box"><a href="#legendary">
     <img alt="1" src="http://i.imgur.com/2nov74U.jpg" class="item-container">
         <div class="item-mask"><div class="item-caption">
-            <h5 class="white">Example</h5><p class="white">legendary</p>
+            <h5 class="white">Afford - $20</h5><p class="white">legendary</p>
                 </div></div></a></div></div>
             
 <div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
-            <h5 class="white">Abut</h5><p class="white">premium</p>
+            <h5 class="white">Aimed - $10</h5><p class="white">premium</p>
                 </div></div></a></div></div>
                                     
 <div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
     <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
         <div class="item-mask"><div class="item-caption">
-            <h5 class="white">Abut</h5><p class="white">premium</p>
+            <h5 class="white">Albany - $10</h5><p class="white">premium</p>
+                </div></div></a></div></div>
+
+<div class="nf-item premium spacing"><div class="item-box"><a href="#premium">
+    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Amatory - $1</h5><p class="white">premium</p>
+                </div></div></a></div></div>
+
+<div class="nf-item lowest spacing"><div class="item-box"><a href="#lowest">
+    <img alt="1" src="http://i.imgur.com/4rxy3eM.jpg" class="item-container">
+        <div class="item-mask"><div class="item-caption">
+            <h5 class="white">Amatory - $1</h5><p class="white">lowest</p>
                 </div></div></a></div></div>
                 
                 <!-------------------------------------------------------------------------- A -------------------------------------------------------------------------->
