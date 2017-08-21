22d9cf6250f17f452abc237a35e7bc3299d379d3
solicited <mitchanimugen@gmail.com>
2017-08-21 17:42:37 +0000

    ok

--- explore.php
+++ explore.php
@@ -10,8 +10,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
 
-   
-
   <!-- CSS -->
     <link href="http://theme.nileforest.com/html/mazel-v1.5/01_mazel_multiprapose/css/style.css" rel="stylesheet" type="text/css" />
     <link href="http://theme.nileforest.com/html/mazel-v1.5/01_mazel_multiprapose/css/bootstrap.css" rel="stylesheet" type="text/css" />
@@ -20,8 +18,6 @@
     <link href="http://theme.nileforest.com/html/mazel-v1.5/01_mazel_multiprapose/css/plugin/sidebar-menu.css" rel="stylesheet" type="text/css" />
     <link href="http://theme.nileforest.com/html/mazel-v1.5/01_mazel_multiprapose/css/plugin/animate.css" rel="stylesheet" type="text/css" />
     <link href="http://theme.nileforest.com/html/mazel-v1.5/01_mazel_multiprapose/css/jquery-ui.css" rel="stylesheet" type="text/css" />
-
-
 </head>
 
 <body>
@@ -34,41 +30,6 @@
     </section>
     <!-- End Preloader -->
 
-    <!-- Search Overlay Menu -->
-    <div class="search-overlay-menu">
-        <span class="search-overlay-close"><i class="ion ion-ios-close-empty"></i></span>
-        <form role="search" id="searchform" action="/search" method="get">
-            <input value="" name="q" type="search" placeholder="Search..." />
-            <button type="submit"><i class="ion ion-ios-search"></i></button>
-        </form>
-    </div>
-    <!-- End Search Overlay Menu -->
-
-    <!-- Sidemenu -->
-    <section id="pushmenu-right" class="pushmenu pushmenu-right side-menu">
-        <a id="menu-sidebar-close-icon" class="menu-close"><i class="ion ion-android-close"></i></a>
-        <h5 class="white">Sign In</h5>
-        <div class="sign-in">
-            <input class="input-sm form-full" type="email" aria-required="true" id="email" name="email" placeholder="Email" value="" />
-            <input class="input-sm form-full" type="password" aria-required="true" id="password" name="password" placeholder="Password" value="" />
-            <input type="submit" class="btn btn-md btn-color-b form-full" value="Sign In" />
-            <a>New Customer?</a>
-        </div>
-        <ul>
-            <li><a class="all-demos-link" target="_blank" href="../demo.html">Main Demo Page</a></li>
-            <li><a href="home.html">Home Default</a></li>
-            <li><a href="portfolio-grid-2col.html">Portfolio</a></li>
-            <li><a href="login-register.html">Login & Signup</a></li>
-            <li><a href="faq-1.html">FAQ</a></li>
-            <li><a href="about-1.html">About</a></li>
-            <li><a href="service-1.html">Service</a></li>
-            <li><a href="blog-grid-3col.html">Blog</a></li>
-            <li><a href="404-error-1.html">404 Error</a></li>
-            <li><a href="shop-checkout.html">Shopping Cart</a></li>
-            <li><a href="contact-1.html">Contact Us</a></li>
-        </ul>
-    </section>
-    <!--End Sidemenu -->
 
     <!-- Site Wraper -->
     <div class="wrapper">
@@ -89,7 +50,6 @@
         </section>
         <!-- End About Section -->
  <?php require 'models/shopmodule.php' ?>
-      
         <!-- End Work Section -->
 
         <div class="spacer-60"></div>
