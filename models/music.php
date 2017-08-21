602bc384f8053654f3d08794849233ad93457286
solicited <mitchanimugen@gmail.com>
2017-08-21 01:26:07 +0000

    music

---
+++ models/music.php
@@ -0,0 +1,19 @@
+<?php echo '
+
+
+
+ <div id="musicplayer" class="box fade-in one">
+  <img src="http://68.media.tumblr.com/tumblr_lkl5w2p3Nn1qfamg6.gif">
+  
+  <div class="play">
+      <embed src="https://www.youtube.com/embed/videoseries?list=PLfNWl8HiMyBqnrlcnUnl-S9oUk-t3VFxL
+      &autoplay=1&start=1&modestbranding=1&autohide=1&showinfo=0&controls=1" 
+      type="text/html" width="100%" height="100%" ></embed>
+      
+  </div><!--play-->
+</div><!--musicplayer-->
+
+
+
+
+'; ?>
