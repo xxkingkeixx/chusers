f232f211422e5af712fe17b673dcbd3f984e8fb7
solicited <mitchanimugen@gmail.com>
2017-08-21 21:05:45 +0000

    ok

--- models/shopmodule.php
+++ models/shopmodule.php
@@ -7,16 +7,31 @@
                 <div class="row">
                     <ul class="col container-filter categories-filter">
                         <li><a class="categories active" data-filter="*">All</a></li>
+                        <li><a class="categories" data-filter=".legendary">legendary</a></li>
                         <li><a class="categories" data-filter=".premium">premium</a></li>
                         <li><a class="categories" data-filter=".regular">regular</a></li>
                         <li><a class="categories" data-filter=".lowest">lowest</a></li>
-                        <!--li><a class="categories" data-filter=".latest">latest</a></li-->
+
                     </ul>
                 </div>
                 <!-- End work Filter -->
                 
          
                 <div class="row container-grid nf-col-5"> <!--how many in a row number-->
+                
+                                    <div class="nf-item legendary spacing">
+                        <div class="item-box">
+                            <a href="portfolio-single7-youtube.html">
+                                <img alt="1" src="http://i.imgur.com/2nov74U.jpg" class="item-container">
+                                <div class="item-mask">
+                                    <div class="item-caption">
+                                        <h5 class="white">Example</h5>
+                                        <p class="white">legendary</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
             
                     <div class="nf-item premium spacing">
                         <div class="item-box">
@@ -63,21 +78,6 @@
                         </div>
                     </div>
 
-
-
-                    <!--div class="nf-item latest spacing">
-                        <div class="item-box">
-                            <a href="portfolio-single7-youtube.html">
-                                <img alt="1" src="http://i.imgur.com/2nov74U.jpg" class="item-container">
-                                <div class="item-mask">
-                                    <div class="item-caption">
-                                        <h5 class="white">Example</h5>
-                                        <p class="white">latest</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div-->
                     
                    
                     
