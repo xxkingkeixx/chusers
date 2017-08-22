89ae740bcfe3413f68ab72840d0bec8b23687c0e
solicited <mitchanimugen@gmail.com>
2017-08-22 15:56:52 +0000

    update

--- test.php
+++ test.php
@@ -5,13 +5,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'simple_html_dom.php';
-$html = file_get_html("https://www.merriam-webster.com/dictionary/dictionary");
+$html = file_get_html("https://www.merriam-webster.com/dictionary/abut");
 
 
-$title = $html->find("div.card-primary-content",0)->plaintext;
-foreach($title as $title){
-    echo '<br>' . $title;
-}
+$title = $html->find("div.card-primary-content",0)->innertext;
+echo $title;
 
 
 
