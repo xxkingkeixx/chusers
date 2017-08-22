028771ebb0813af8525bd1e9be2f7ba718a4604c
solicited <mitchanimugen@gmail.com>
2017-08-22 15:25:20 +0000

    update

--- test.php
+++ test.php
@@ -5,17 +5,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'simple_html_dom.php';
-$url = 'https://www.merriam-webster.com/dictionary/dictionary';
-	$page = file_get_contents($url);
-	$page = mb_convert_encoding($page, 'utf-8', mb_detect_encoding($page));
-	$page = mb_convert_encoding($page, 'html-entities', 'utf-8'); 
-	$dom = new DOMDocument();
-	libxml_use_internal_errors(true);
-	$dom->loadHTML($page);
-	libxml_use_internal_errors(false);
-	$contentSection = $dom->find('.definition-inner-item with-sense');
-	$pageContent = $dom->saveHTML($contentSection);
-	echo $pageContent;	
+$html = file_get_html("http://www.wikihow.com/Adjust-Bass-on-Computer");
+if($html){
+    echo 'ok';
+}
+else
+    echo 'um..';
+    
+
+$title = $html->find("p.definition-inner-item with-sense",0)->innertext;
+echo $title;
 
 
 
