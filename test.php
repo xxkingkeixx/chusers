07f444ecec369692906992706254e3f53ffb2dab
solicited <mitchanimugen@gmail.com>
2017-08-22 15:40:20 +0000

    update

--- test.php
+++ test.php
@@ -5,19 +5,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'simple_html_dom.php';
+$html = file_get_html("https://www.merriam-webster.com/dictionary/dictionary");
 
 
-$url = 'https://www.merriam-webster.com/dictionary/dictionary';
-	$page = file_get_contents($url);
-	$page = mb_convert_encoding($page, 'utf-8', mb_detect_encoding($page));
-	$page = mb_convert_encoding($page, 'html-entities', 'utf-8'); 
-	$dom = new DOMDocument();
-	libxml_use_internal_errors(true);
-	$dom->loadHTML($page);
-	libxml_use_internal_errors(false);
-	$contentSection = $dom->getElementById('definition-inner-item with-sense');
-	$pageContent = $dom->saveHTML($contentSection);
-	echo $pageContent;	
+$title = $html->find("div#card-primary-content",0)->innertext;
+echo $title;
 
 
 
