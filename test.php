e40ac63cb337ef84283d523f0d4b1deb3c16846c
solicited <mitchanimugen@gmail.com>
2017-08-22 23:24:32 +0000

    ok

--- test.php
+++ test.php
@@ -14,10 +14,13 @@ $row = mysqli_query($conn, "SELECT user_name FROM (
 while ($lastusers = mysqli_fetch_array($row)) {
     echo '<br>' .$lastusers["user_name"];
     echo '<br>Definition:<br>';
-    $html = file_get_html("https://www.merriam-webster.com/dictionary/" . $lastusers["user_name"] );
+    $html = @file_get_html("https://www.merriam-webster.com/dictionary/" . $lastusers["user_name"] );
     $title = $html->find("div.card-primary-content",0)->innertext;
     echo $title;
-
+    
+      //save definitions to corresponding username 
+    $save = mysqli_query($conn, 'UPDATE store SET def = $title WHERE  user_name = $lastusers["user_name"]');
+     mysqli_query($conn,$save);
     
     
 }
