98998dc7c2d9c00cf2179de9620aa5f0e2bf73c2
solicited <mitchanimugen@gmail.com>
2017-08-22 15:34:44 +0000

    update

--- test.php
+++ test.php
@@ -5,20 +5,19 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'simple_html_dom.php';
-$html = file_get_html("https://www.merriam-webster.com/dictionary/dictionary");
-if($html){
-    echo 'ok';
-}
-else
-    echo 'um..';
-    
 
-$title = $html->find(".definition-inner-item with-sense")->innertext;
-echo $title;
-if ($title) {
-    echo $title[0]->plaintext;
-}
 
+$url = 'http://www.dictionary.com/browse/banausic';
+	$page = file_get_contents($url);
+	$page = mb_convert_encoding($page, 'utf-8', mb_detect_encoding($page));
+	$page = mb_convert_encoding($page, 'html-entities', 'utf-8'); 
+	$dom = new DOMDocument();
+	libxml_use_internal_errors(true);
+	$dom->loadHTML($page);
+	libxml_use_internal_errors(false);
+	$contentSection = $dom->getElementsById('def-list');
+	$pageContent = $dom->saveHTML($contentSection);
+	echo $pageContent;	
 
 
 
