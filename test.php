11c50eef9ada67320c777305d4226916f351e522
solicited <mitchanimugen@gmail.com>
2017-08-22 15:26:47 +0000

    update

--- test.php
+++ test.php
@@ -13,7 +13,7 @@ else
     echo 'um..';
     
 
-$title = $html->find("p.definition-inner-item with-sense",0)->innertext;
+$title = $html->find(".definition-inner-item with-sense",0)->innertext;
 echo $title;
 
 
