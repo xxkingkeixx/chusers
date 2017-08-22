fb8d8265af3a665868fff6b4b9c58e896066c17a
solicited <mitchanimugen@gmail.com>
2017-08-22 15:44:08 +0000

    update

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ include 'simple_html_dom.php';
 $html = file_get_html("https://www.merriam-webster.com/dictionary/dictionary");
 
 
-$title = $html->find("p.definition-inner-item with-sense",1)->innertext;
+$title = $html->find("div.card-primary-content",1)->innertext;
 echo $title;
 
 
