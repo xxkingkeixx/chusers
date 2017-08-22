f0613ce8762ee8e10b0544dae0d7a82a7311ba0c
solicited <mitchanimugen@gmail.com>
2017-08-22 15:37:10 +0000

    update

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 include 'simple_html_dom.php';
 
 
-$url = 'http://www.dictionary.com/browse/banausic';
+$url = 'https://www.merriam-webster.com/dictionary/dictionary';
 	$page = file_get_contents($url);
 	$page = mb_convert_encoding($page, 'utf-8', mb_detect_encoding($page));
 	$page = mb_convert_encoding($page, 'html-entities', 'utf-8'); 
@@ -15,7 +15,7 @@ $url = 'http://www.dictionary.com/browse/banausic';
 	libxml_use_internal_errors(true);
 	$dom->loadHTML($page);
 	libxml_use_internal_errors(false);
-	$contentSection = $dom->getElementById('def-list');
+	$contentSection = $dom->getElementById('definition-inner-item with-sense');
 	$pageContent = $dom->saveHTML($contentSection);
 	echo $pageContent;	
 
