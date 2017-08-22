5fe9b2ffa7fe02d88ba2f72f9986c245d3fbe820
solicited <mitchanimugen@gmail.com>
2017-08-22 16:49:21 +0000

    k

--- checkout/legendary.php
+++ checkout/legendary.php
@@ -20,7 +20,7 @@
 <td valign="top"><label for="first_name">First Name *</label></td>
 <td valign="top"><input  type="text" name="first_name" maxlength="50" size="30"></td></tr>
 <tr>
-<td valign="top""><label for="last_name">Last Name *</label></td>
+<td valign="top""><label for="last_name">Name*</label></td>
 <td valign="top"><input  type="text" name="last_name" maxlength="50" size="30"></td></tr>
 <tr>
 <td valign="top"><label for="email">Email Address *</label></td>
