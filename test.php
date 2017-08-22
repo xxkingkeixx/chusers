f30bd8c6adebccfd5e74213f0325d6fe838132e4
solicited <mitchanimugen@gmail.com>
2017-08-22 16:06:00 +0000

    update

--- test.php
+++ test.php
@@ -1,15 +1,28 @@
 <?php 
-
-
+include 'db.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'simple_html_dom.php';
-$html = file_get_html("https://www.merriam-webster.com/dictionary/abut");
+
+$row = mysqli_query($conn, "SELECT user_name FROM (
+  SELECT * 
+  FROM store 
+  ORDER BY user_id DESC
+) AS store ORDER BY user_id ASC");
+
+while ($lastusers = mysqli_fetch_array($row)) {
+    echo '<br>' .$lastusers["user_name"];
+    echo '<br>Definition:<br>';
+    $html = file_get_html("https://www.merriam-webster.com/dictionary/" . $lastusers["user_name"] );
+    $title = $html->find("div.card-primary-content",0)->innertext;
+    echo $title;
+
+    
+    
+}
 
 
-$title = $html->find("div.card-primary-content",0)->innertext;
-echo $title;
 
 
 
