e17838705c0de96849b4c6cfb809ea118bf1c920
solicited <mitchanimugen@gmail.com>
2017-08-22 16:52:53 +0000

    k

--- checkout/legendary.php
+++ checkout/legendary.php
@@ -20,13 +20,10 @@
 <td valign="top"><label for="name">*NAME</label></td>
 <td valign="top"><input  type="text" name="name" maxlength="50" size="30"></td></tr>
 <tr>
-<td valign="top"><label for="email">Email Address *</label></td>
+<td valign="top"><label for="email">*EMAIL</label></td>
 <td valign="top"><input  type="text" name="email" maxlength="80" size="30"></td></tr>
 <tr>
-<td valign="top"><label for="telephone">Telephone Number</label></td>
-<td valign="top"><input  type="text" name="telephone" maxlength="30" size="30"></td></tr>
-<tr>
-<td valign="top"><label for="comments">Comments *</label></td>
+<td valign="top"><label for="comments">*ACCOUNT REQUESTED / ACCOUNT TO CONTACT YOU</label></td>
 <td valign="top"><textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea></td></tr>
 <tr>
 <td colspan="2" style="text-align:center"><input type="submit" value="Submit">
