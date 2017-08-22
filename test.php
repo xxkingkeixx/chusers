35b0fc5d4920457b11e7867639b630d8e1d815a1
solicited <mitchanimugen@gmail.com>
2017-08-22 15:07:36 +0000

    update

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include '/simple_html_dom.php';
+include 'simple_html_dom.php';
 $html = file_get_html("http://www.wikihow.com/Adjust-Bass-on-Computer");
 if($html){
     echo 'ok';
