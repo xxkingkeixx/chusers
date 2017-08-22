c40300edf56466f52157d2a76db69b55f0046e81
solicited <mitchanimugen@gmail.com>
2017-08-22 15:15:43 +0000

    update

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'simple_html_dom.php';
-$url = 'http://www.dictionary.com/browse/banausic';
+$url = 'https://www.merriam-webster.com/dictionary/dictionary';
 	$page = file_get_contents($url);
 	$page = mb_convert_encoding($page, 'utf-8', mb_detect_encoding($page));
 	$page = mb_convert_encoding($page, 'html-entities', 'utf-8'); 
@@ -13,7 +13,7 @@ $url = 'http://www.dictionary.com/browse/banausic';
 	libxml_use_internal_errors(true);
 	$dom->loadHTML($page);
 	libxml_use_internal_errors(false);
-	$contentSection = $dom->getElementsByClassName('def-list');
+	$contentSection = $dom->getElementById('card-primary-content');
 	$pageContent = $dom->saveHTML($contentSection);
 	echo $pageContent;	
 
