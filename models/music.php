fd66fc6be04227fa27f23439039c42d542fad541
solicited <mitchanimugen@gmail.com>
2017-08-23 01:24:45 +0000

    ok

--- models/music.php
+++ models/music.php
@@ -5,9 +5,10 @@
  <div id="musicplayer" class="box fade-in one">
   <img src="http://68.media.tumblr.com/tumblr_lkl5w2p3Nn1qfamg6.gif">
   
-  <div class="play">
-      <embed src="https://www.youtube.com/embed/videoseries?list=PLSWJOy5Jg5IsAyrq1Anwjk1VpsU7RCtNf&index=1
-      &autoplay=1&start=0&modestbranding=1&autohide=1&showinfo=0&controls=1" 
+  <div class="play">';
+      echo '<embed src="https://www.youtube.com/embed/videoseries?list=PLSWJOy5Jg5IsAyrq1Anwjk1VpsU7RCtNf&index=';
+      echo rand(0,1);
+      echo '&autoplay=1&start=0&modestbranding=1&autohide=1&showinfo=0&controls=1" 
       type="text/html" width="100%" height="100%" ></embed>
       
   </div><!--play-->
