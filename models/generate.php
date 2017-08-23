12dcfcceaa3258ec9297547899de9575b07bd769
solicited <mitchanimugen@gmail.com>
2017-08-23 20:40:14 +0000

    clear

--- models/generate.php
+++ models/generate.php
@@ -1,22 +1,33 @@
  <?php            
-
+//require db connection
 require 'db.php';
+//select all from db
 $row = mysqli_query($conn, "SELECT * FROM store ");
-$usersep = mysqli_query($conn, "SELECT user_name FROM store ");
 
+//while each username is fetched
 while ($rowdata = mysqli_fetch_array($row)  ) {
+    //set letter to the username
     $letter = $rowdata['user_name'];
+    //echo out html
     echo '<div class="nf-item ';
+    //get whether its legendary etc
     echo $rowdata['class'] . " ";
+    //get the first letter of the word
     echo $letter[0];  
+    //print html
     echo ' spacing"><div class="item-box"><a href="#premium">
-    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
+    <img alt="1" src="images/flower.png" class="item-container">
     <div class="item-mask"><div class="item-caption">
     <h5 class="white">';
+    //print the username
     echo $rowdata["user_name"];
+    //print a dollar sign
     echo ' - $';
+    //print price from database
     echo $rowdata['price'];
+    //print html
     echo'</h5><p class="white">';
+    //print class again
     echo $rowdata['class'];
     echo'</p></div></div></a></div></div>';
     
