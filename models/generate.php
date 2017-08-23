51a7649489fcb628b51113c1732378c004322dc2
solicited <mitchanimugen@gmail.com>
2017-08-23 16:33:28 +0000

    clear

--- models/generate.php
+++ models/generate.php
@@ -5,8 +5,8 @@ $usersep = mysqli_query($conn, "SELECT user_name FROM store ");
 
 while ($rowdata = mysqli_fetch_array($row) && $usersplit = mysqli_fetch_array($usersep) ) {
     $letter = $usersplit[0];
-    
-    echo $lastusers["user_name"] . ",  ";
+    echo $letter;
+    echo $rowdata["user_name"] . ",  ";
 }
 
 echo '
