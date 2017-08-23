c5e7cb8765e81bfa4a80d6626e175d1ea3afa258
solicited <mitchanimugen@gmail.com>
2017-08-23 11:46:47 +0000

    update

--- search_results.php
+++ search_results.php
@@ -2,25 +2,25 @@
 <?php require 'navbar.php' ?>
 <?php require 'db.php'     ?>
 
-<?php echo '	
+<?php 
+echo '	
 <!-- Banner -->
 <section id="banner">
 <div class="content">
 <header>
 <h2>Matching Usernames</h2>
 <p>Results: ' ;
-$query=mysqli_query($conn,"SELECT * FROM store WHERE user_name='$user_name' ");
-$count=mysqli_num_rows($query);
-$row=mysqli_fetch_array($query);
-echo $count;					
+
+//$query=mysqli_query($conn,"SELECT * FROM store WHERE user_name='$user_name' ");
+//$count=mysqli_num_rows($query);
+//$row=mysqli_fetch_array($query);
+//echo $count;					
 							
 echo '</p>
-						</header>
-						<span class="image"><img src="/images/search.jpg" alt="" /></span>
-					</div>
-					
-				</section>
-';
+</header>
+<span class="image"><img src="/images/search.jpg" alt="" /></span>
+</div>
+</section> ';
 
 ?>
 
