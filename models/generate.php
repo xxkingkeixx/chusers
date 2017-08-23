1cd6fa27d8ab9e67af9e86a506f5cb1b90a08707
solicited <mitchanimugen@gmail.com>
2017-08-23 16:38:24 +0000

    clear

--- models/generate.php
+++ models/generate.php
@@ -1,5 +1,6 @@
  <?php            
-require '/db.php';
+
+require 'db.php';
 $row = mysqli_query($conn, "SELECT * FROM store ");
 $usersep = mysqli_query($conn, "SELECT user_name FROM store ");
 
@@ -9,14 +10,6 @@ while ($rowdata = mysqli_fetch_array($row) && $usersplit = mysqli_fetch_array($u
     echo $rowdata["user_name"] . ",  ";
 }
 
-echo '
-                
-                <div class="nf-item premium d spacing"><div class="item-box"><a href="#premium">
-    <img alt="1" src="http://i.imgur.com/DVAxSJV.png" class="item-container">
-        <div class="item-mask"><div class="item-caption">
-            <h5 class="white">Example - $10</h5><p class="white">premium</p>
-                </div></div></a></div></div>
-      ';          
-                
+    
                 
 ?>                
