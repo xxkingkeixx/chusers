b29ad6e3470ba87d793878c3648cfe3c9eed1940
solicited <mitchanimugen@gmail.com>
2017-08-23 16:53:51 +0000

    clear

--- models/generate.php
+++ models/generate.php
@@ -4,8 +4,8 @@ require 'db.php';
 $row = mysqli_query($conn, "SELECT * FROM store ");
 $usersep = mysqli_query($conn, "SELECT user_name FROM store ");
 
-while ($rowdata = mysqli_fetch_array($row) && $usersplit = mysqli_fetch_array($usersep) ) {
-    $letter = $usersplit['user_name'];
+while ($rowdata = mysqli_fetch_array($row)  ) {
+    $letter = $rowdata['user_name'];
     echo $letter[0];
     //echo $rowdata["user_name"] . ",  ";
 }
