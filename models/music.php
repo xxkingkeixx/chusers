7a5a763d7d6fe1c0e24d678b0ce351f5518044c1
solicited <mitchanimugen@gmail.com>
2017-08-23 00:56:33 +0000

    ok

--- models/music.php
+++ models/music.php
@@ -6,8 +6,8 @@
   <img src="http://68.media.tumblr.com/tumblr_lkl5w2p3Nn1qfamg6.gif">
   
   <div class="play">
-      <embed src="https://www.youtube.com/playlist?list=PLSWJOy5Jg5IsAyrq1Anwjk1VpsU7RCtNf
-      &autoplay=1&start=0&modestbranding=1&autohide=1&showinfo=0&controls=1" 
+      <embed src="https://www.youtube.com/embed/videoseries?list=PLSWJOy5Jg5IsAyrq1Anwjk1VpsU7RCtNf
+      &autoplay=1&start=230&modestbranding=1&autohide=1&showinfo=0&controls=1" 
       type="text/html" width="100%" height="100%" ></embed>
       
   </div><!--play-->
