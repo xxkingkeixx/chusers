232c297df9a6c6bc1464c2ba48e3dc7bb27f277e
solicited <mitchanimugen@gmail.com>
2017-08-23 11:35:48 +0000

    update

--- models/navbar.php
+++ models/navbar.php
@@ -8,7 +8,7 @@
 					<nav id="nav" style="margin-right:40px;">
 						<ul>
 							<li id="list"><a href="index.html" class="fa fa-home fa-fw"></a></li>
-							<li id="list"> <input id="search" type="text" style ="border-radius:33px;height:25px;" method="post" name="user_name" maxlength="50" action="search_results.php" class="form-control" placeholder="Search for Usernames" /></li>
+							<li id="list"> <input id="search" type="text" style ="border-radius:33px;height:25px;" method="post" name="user_name" maxlength="50" action="/search_results.php" class="form-control" placeholder="Search for Usernames" /></li>
 						<!-- Developers -->
 							<li id="list">
 								<a href="/">Developers</a>
