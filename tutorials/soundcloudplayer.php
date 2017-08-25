964d14464082f63411ff76fdd1f86afb00a17e27
solicited <mitchanimugen@gmail.com>
2017-08-25 01:44:40 +0000

    added tutorials

--- tutorials/soundcloudplayer.php
+++ tutorials/soundcloudplayer.php
@@ -1,1 +1,176 @@
-sc player test
+
+<!DOCTYPE html>
+<head>
+ 
+    <title>Tutorials</title>
+    <link rel="shortcut icon" href="https://1001freedownloads.s3.amazonaws.com/vector/thumb/126060/Anonymous_aiga_information_.png">
+    <link rel="alternate" type="application/rss+xml" href="{RSS}">
+   
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+<script src="http://static.tumblr.com/iuw14ew/VSQma1786/jquery.style-my-tooltips.js"></script>
+ 
+<script>
+ 
+(function($){
+ 
+$(document).ready(function(){
+ 
+$("a[title]").style_my_tooltips({
+ 
+tip_follows_cursor:true,
+ 
+tip_delay_time:100,
+ 
+tip_fade_speed:300,
+ 
+attribute:"title"
+ 
+});
+ 
+});
+ 
+})(jQuery);
+ 
+</script>
+
+<script>
+$(document).ready(function(){
+  $(".what").click(function(){
+        $('.img').toggleClass('top');
+        $(this).toggleClass('rotate')
+  });
+});
+</script>
+ 
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,400' rel='stylesheet' type='text/css'>
+ 
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+   
+    <style type="text/css">
+ 
+#s-m-t-tooltip {
+    max-width:150px;
+    padding:4px 6px;
+    margin:20px 14px 7px 10px;
+    background-color:#fff;
+    font-family:Calibri;
+    font-size:10px;
+    line-height:11px;
+    letter-spacing:1px;
+    border:1px solid #aaa;
+    text-transform:uppercase;
+    color:#777;
+    z-index:99999999;
+}
+   
+::-webkit-scrollbar {
+    height:0px;
+    width:3px;
+    background-color: transparent;
+}
+   
+::-webkit-scrollbar-thumb {
+    background-color: #555; /*scrollbar color*/
+}
+ 
+::selection {
+    background-color: #000;
+    color: #fff;
+}
+   
+body {
+    background-color: #eee; /*page background color*/
+    font-family:arial;
+    font-size:12px;
+}
+     
+a {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color: #bbb; /*link color*/
+}
+   
+a:hover {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color:#666; /*link hover color*/
+}
+
+#con {
+    width:750px;
+    height:520px;
+    overflow:hidden;
+    margin:auto;
+    position:fixed;
+    background:#fff; /*content background*/
+    left:0;
+    border-radius:3px;
+    right:0;
+    top:0;
+    bottom:0;
+}
+
+
+.content {
+    height:450px;
+    padding:0px 50px 50px;
+    overflow:auto;
+    color:#777; /*content color*/
+    line-height:170%;
+}
+
+.content p {
+    margin:25px 0px;
+}
+
+.content p:first-of-type {
+    margin-top:5px;
+}
+
+h1{
+background:#transparent;
+text-transform:uppercase;
+margin-bottom:2px;
+border-bottom:1px dotted #000000;
+color:#000000;
+font-size:9px;
+font-family: 'Verdana', serif;
+}
+
+h2{
+background:#transparent;
+text-transform:uppercase;
+margin-bottom:2px;
+border-bottom:0px dotted #000000;
+color:#000000;
+font-size:9px;
+font-family: 'Verdana', serif;
+}
+
+</style>
+</head>
+<body>
+
+<div id="con">
+    <div class="content">
+<!--content begin-->
+<br><br>
+
+<p><h1>Music Player Alternative #2: Soundcloud Autoplay</h1></p>
+
+
+<p><b>1. Pick any soundcloud song. All you need is the embed code provided. To get the embed code, click the “SHARE” button.</b></p><figure data-orig-width="601" data-orig-height="476" class="tmblr-full"><img src="https://68.media.tumblr.com/9065280f574aa4709afd9ae21e49e3e0/tumblr_inline_oauhik6mb81rzyom4_540.png" alt="image" data-orig-width="601" data-orig-height="476"></figure><p><b>2. Click on the embed tab and copy the entire iframe code.</b></p><figure data-orig-width="555" data-orig-height="433" class="tmblr-full"><img src="https://68.media.tumblr.com/8b1453f6aa2cbec9eb204ee4d74052a9/tumblr_inline_oauhitq9HP1rzyom4_540.png" alt="image" data-orig-width="555" data-orig-height="433"></figure><p><b>3. This is where you’ll need to edit a bit. Copy the code into a notepad or something similar so you can clearly see all of the code.</b></p><p><i>This is the ORIGINAL code example;</i></p><p><i>&lt;iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/261193806&amp;amp;auto_play=false&amp;amp;hide_related=false&amp;amp;show_comments=true&amp;amp;show_user=true&amp;amp;show_reposts=false&amp;amp;visual=true"&gt;&lt;/iframe&gt;</i></p><p><b>4. Set your <i>height=“0″ </i>and replace the FALSE in <i>auto_play=false </i>to TRUE.</b></p><p><i>You should result in a code similar to this:</i></p><p><i>&lt;iframe width="100%" </i><b>height="0"</b><i> scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/261193806&amp;amp;auto_play=</i><b>TRUE</b><i>&amp;amp;hide_related=false&amp;amp;show_comments=true&amp;amp;show_user=true&amp;amp;show_reposts=false&amp;amp;visual=true"&gt;&lt;/iframe&gt;</i> </p><p><b>5. The code should then be put under the section within your profile (if it is made by me) called &lt;!— MUSIC PLAYER —&gt;. If you cannot find this section, try Ctrl+F and manually enter in the name of the section.</b></p><p><b><i>The results of the player can be seen <a href="http://hngthemes.tumblr.com/soundcloudplayer">HERE.</a></i></b><br></p><p><i>**This player may cause you to be “buried” in the Meet People section. If you’re perfectly fine with that, this player is fine to use. If you want people to message you constantly, use one of the other two.</i></p>
+
+<!--content end-->
+    </div>
+</div>
+
+ 
+   
+</body>
+</html>
