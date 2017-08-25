386506a4352c1bee41922b331646df572c7fe223
solicited <mitchanimugen@gmail.com>
2017-08-25 22:24:05 +0000

    ok

--- index.php
+++ index.php
@@ -83,9 +83,8 @@
 					</div>
 					<?php require 'models/feed.php' ?>
 						<footer class="major">
-							<ul class="actions">
-								<li><a href="/explore" class="button">Explore Available Accounts</a></li>
-							</ul>
+
+
 						</footer>
 					</div>
 				</section>
