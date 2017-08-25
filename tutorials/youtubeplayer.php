b6fe90c2252c2a951fa0146fdec5702ffb37bc3b
solicited <mitchanimugen@gmail.com>
2017-08-25 01:42:05 +0000

    added tutorials

--- tutorials/youtubeplayer.php
+++ tutorials/youtubeplayer.php
@@ -1,2 +1,186 @@
 
-yt player test
+<!DOCTYPE html>
+<head>
+ 
+    <title>Tutorials</title>
+    <link rel="shortcut icon" href="https://1001freedownloads.s3.amazonaws.com/vector/thumb/126060/Anonymous_aiga_information_.png">
+    <link rel="alternate" type="application/rss+xml" href="{RSS}">
+   
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+<script src="http://static.tumblr.com/iuw14ew/VSQma1786/jquery.style-my-tooltips.js"></script>
+ 
+<script>
+ 
+(function($){
+ 
+$(document).ready(function(){
+ 
+$("a[title]").style_my_tooltips({
+ 
+tip_follows_cursor:true,
+ 
+tip_delay_time:100,
+ 
+tip_fade_speed:300,
+ 
+attribute:"title"
+ 
+});
+ 
+});
+ 
+})(jQuery);
+ 
+</script>
+
+<script>
+$(document).ready(function(){
+  $(".what").click(function(){
+        $('.img').toggleClass('top');
+        $(this).toggleClass('rotate')
+  });
+});
+</script>
+ 
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,400' rel='stylesheet' type='text/css'>
+ 
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+   
+    <style type="text/css">
+ 
+#s-m-t-tooltip {
+    max-width:150px;
+    padding:4px 6px;
+    margin:20px 14px 7px 10px;
+    background-color:#fff;
+    font-family:Calibri;
+    font-size:10px;
+    line-height:11px;
+    letter-spacing:1px;
+    border:1px solid #aaa;
+    text-transform:uppercase;
+    color:#777;
+    z-index:99999999;
+}
+   
+::-webkit-scrollbar {
+    height:0px;
+    width:3px;
+    background-color: transparent;
+}
+   
+::-webkit-scrollbar-thumb {
+    background-color: #555; /*scrollbar color*/
+}
+ 
+::selection {
+    background-color: #000;
+    color: #fff;
+}
+   
+body {
+    background-color: #eee; /*page background color*/
+    font-family:arial;
+    font-size:12px;
+}
+     
+a {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color: #bbb; /*link color*/
+}
+   
+a:hover {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color:#666; /*link hover color*/
+}
+
+#con {
+    width:750px;
+    height:520px;
+    overflow:hidden;
+    margin:auto;
+    position:fixed;
+    background:#fff; /*content background*/
+    left:0;
+    border-radius:3px;
+    right:0;
+    top:0;
+    bottom:0;
+}
+
+
+.content {
+    height:450px;
+    padding:0px 50px 50px;
+    overflow:auto;
+    color:#777; /*content color*/
+    line-height:170%;
+}
+
+.content p {
+    margin:25px 0px;
+}
+
+.content p:first-of-type {
+    margin-top:5px;
+}
+
+h1{
+background:#transparent;
+text-transform:uppercase;
+margin-bottom:2px;
+border-bottom:1px dotted #000000;
+color:#000000;
+font-size:9px;
+font-family: 'Verdana', serif;
+}
+
+h2{
+background:#transparent;
+text-transform:uppercase;
+margin-bottom:2px;
+border-bottom:0px dotted #000000;
+color:#000000;
+font-size:9px;
+font-family: 'Verdana', serif;
+}
+
+</style>
+</head>
+<body>
+
+<div id="con">
+    <div class="content">
+<!--content begin-->
+<br><br>
+
+<p><h1>Recommended Chatango Music Player: Youtube Autoplay</h1></p>
+
+<p><strong>1. Choose any video from youtube.</strong></p>
+<p><strong>2. Get its embed code.</strong></p>
+<p><strong>3. Grab the embed code of the video you want.</strong>
+<img src="https://68.media.tumblr.com/c5c7d0458a7345121bc172d69c4ce1ed/tumblr_inline_oauht9LG3q1rzyom4_540.png" alt="image"></p>
+<p><strong>4. Ignore everything except for the embed song url.</strong></p>
+<p>&lt;iframe width=“0” height=“0” src=“<strike><strong>EMBEDCODEHERE</strong></strike>?autoplay=1” frameborder=“0” allowfullscreen&gt;&lt;/iframe&gt;</p>
+<p><strong>5. Replace the <strong><strike></strong>EMBEDCODEHERE</strong></strike>** text with your actual embed code and paste the entire iframe script into your “About Me” section of your “Edit Profile”.**</p>
+<p>In this example, you’d put..</p>
+<p><em>&lt;iframe width=“0″ height=“0″ src=“<a href="https://www.youtube.com/embed/a8GRiiaef3s">https://www.youtube.com/embed/a8GRiiaef3s</a>?autoplay=1″ frameborder=“0″ allowscreen&gt;&lt;/iframe&gt;</em></p>
+<p><strong>6. And, you’re done.</strong></p>
+
+
+
+<!--content end-->
+    </div>
+</div>
+
+ 
+   
+</body>
+</html>
