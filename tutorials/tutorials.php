62ce3616ca5766dc8755454b1859a99237c4be21
solicited <mitchanimugen@gmail.com>
2017-08-25 01:12:41 +0000

    added tutorials

--- tutorials/tutorials.php
+++ tutorials/tutorials.php
@@ -1,5 +1,350 @@
 <?php echo '
 
+<!DOCTYPE html>
+<head>
+ 
+    <title>Tutorials</title>
+    <link rel="shortcut icon" href="https://1001freedownloads.s3.amazonaws.com/vector/thumb/126060/Anonymous_aiga_information_.png">
+    <link rel="alternate" type="application/rss+xml" href="{RSS}">
+   
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+<script src="http://static.tumblr.com/iuw14ew/VSQma1786/jquery.style-my-tooltips.js"></script>
+ 
+<script>
+ 
+(function($){
+ 
+$(document).ready(function(){
+ 
+$("a[title]").style_my_tooltips({
+ 
+tip_follows_cursor:true,
+ 
+tip_delay_time:100,
+ 
+tip_fade_speed:300,
+ 
+attribute:"title"
+ 
+});
+ 
+});
+ 
+})(jQuery);
+ 
+</script>
+
+<script>
+$(document).ready(function(){
+  $(".what").click(function(){
+        $('.img').toggleClass('top');
+        $(this).toggleClass('rotate')
+  });
+});
+</script>
+ 
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,400' rel='stylesheet' type='text/css'>
+ 
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+   
+    <style type="text/css">
+ 
+#s-m-t-tooltip {
+    max-width:150px;
+    padding:4px 6px;
+    margin:20px 14px 7px 10px;
+    background-color:#fff;
+    font-family:Calibri;
+    font-size:10px;
+    line-height:11px;
+    letter-spacing:1px;
+    border:1px solid #aaa;
+    text-transform:uppercase;
+    color:#777;
+    z-index:99999999;
+}
+   
+::-webkit-scrollbar {
+    height:0px;
+    width:3px;
+    background-color: transparent;
+}
+   
+::-webkit-scrollbar-thumb {
+    background-color: #555; /*scrollbar color*/
+}
+ 
+::selection {
+    background-color: #000;
+    color: #fff;
+}
+   
+body {
+    background-color: #eee; /*page background color*/
+    font-family:arial;
+    font-size:12px;
+}
+     
+a {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color: #bbb; /*link color*/
+}
+   
+a:hover {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color:#666; /*link hover color*/
+}
+
+#con {
+    width:750px;
+    height:520px;
+    overflow:hidden;
+    margin:auto;
+    position:fixed;
+    background:#fff; /*content background*/
+    left:0;
+    border-radius:3px;
+    right:0;
+    top:0;
+    bottom:0;
+}
+
+.img {
+    background:url(http://i.imgur.com/zcF9sKv.jpg); /*display image*/
+    background-size:cover;
+    height:450px;
+    opacity:0.9;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+}
+
+.img.top {
+    margin-top:-450px;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+}
+
+.title {
+    height:50px;
+    line-height:30px;
+    font-size:15px;
+    padding:0px 50px;
+    font-style:italic;
+    font-family:'Raleway', sans-serif;
+    text-transform:uppercase;
+    letter-spacing:3px;
+    margin-top:20px;
+    
+    overflow:hidden;
+    box-sizing:border-box;
+    color:#ccc; /*title color*/
+    word-spacing:10px;
+}
+
+.what {
+    float:right;
+    cursor:pointer;
+    font-size:40px;
+    color:#777; /*arrow color*/
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+}
+
+.what:hover {
+    color:#aaa; /*arrow hover color*/
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+}
+
+.what.rotate {
+    transform:rotate(180deg);
+    -webkit-transform:rotate(180deg);
+    -moz-transform:rotate(180deg);
+    -o-transform:rotate(180deg);
+}
+
+.content {
+    height:450px;
+    padding:0px 50px 50px;
+    overflow:auto;
+    color:#777; /*content color*/
+    line-height:170%;
+}
+
+.content p {
+    margin:25px 0px;
+}
+
+.content p:first-of-type {
+    margin-top:5px;
+}
+
+h1{
+background:#transparent;
+text-transform:uppercase;
+margin-bottom:2px;
+border-bottom:1px dotted #000000;
+color:#000000;
+font-size:9px;
+font-family: 'Verdana', serif;
+}
+
+</style>
+</head>
+<body>
+
+<div id="con">
+        <div class="img"></div>
+        <div class="title">Tutorials <!---title--->
+        <div class="what"><i class="fa fa-angle-down"></i></div>
+        </div>
+    
+    <div class="content">
+<!--content begin-->
+<h1>Music Players</h1>
+
+<blockquote>
+<p><strong>How to Convert a Youtube Video to Mp3:</strong></p>
+<p><em><a href="http://chatango-themes.tumblr.com/post/147913765213/how-to-convert-a-youtube-video-into-an-mp3-file" target="_blank">Tutorial Here</a>&nbsp;(Recommended For Mini and Billy Player)</em></p>
+<p><strong>Recommended Music Player:</strong></p>
+<p><em><a href="http://chatango-themes.tumblr.com/post/147919526273/recommended-chatango-music-player-youtube" target="_blank">Youtube</a>&nbsp;(Autoplay)</em></p>
+<p><strong>Alternatives:</strong></p>
+<p><em><a href="http://chatango-themes.tumblr.com/post/147918959098/music-player-alternative-1-mini-flash" target="_blank">Mini Flash</a> (Manual Start)</em></p>
+<p><em><a href="http://chatango-themes.tumblr.com/post/147919436593/music-player-alternative-3-billy-sheep" target="_blank">Billy Player</a> (Manual/Autoplay)</em></p>
+<p><em><a href="http://chatango-themes.tumblr.com/post/147919214368/music-player-alternative-2-soundcloud" target="_blank">Soundcloud</a> (Autoplay)</em></p>
+</blockquote>
+
+
+
+
+
+
+
+
+
+
+<h1>Text Coding</h1>
+<blockquote>
+<b>How To Change Color:</b> &lt;font color=“#HEXCODE”>
+<br><i>EXAMPLE: &lt;font color=“#000000”>TEXT&lt;/font></i>
+<p><p>
+<b>Change Font Size:</b> &lt;font size=“number”>
+<br><i>EXAMPLE: &lt;font size=“20”></i>
+<p>
+<b>Change Font:</b> &lt;font face=“font name” >
+<br><i>EXAMPLE: &lt;font face=“verdana”></i>
+<p>
+<b>Font Styles:</b>
+<br><u>Underline Text:</u> &lt;u>TEXT&lt;/u>
+<br>
+<b>Bold Text:</b> &lt;b>TEXT&lt;/b>
+<br><strike>Strike Text:</strike> &lt;strike>TEXT&lt;/strike>
+<p>
+<b>Set Text Position:</b> &lt;p align=“left/right/center”>
+<br>
+<i>EXAMPLES: 
+<br>
+&lt;p align="left"><br>
+&lt;p align="right"><br>
+&lt;p align="center"></i>
+</blockquote>
+
+
+
+
+<h1>Profile Coding</h1>
+<blockquote>
+<b>Hiding Ads:</b>
+<br>
+&lt;Style&gt; .logo_div { visibility: hidden; } .fpix_header { visibility: hidden; } .topad { visibility: hidden; } .fpix_asl_table { visibility: hidden; } .profile_text { visibility: hidden; } .nc_profile { visibility: visible; } #left_container { visibility: hidden } #profilephoto { visibility: hidden; } #flash_close_butt { visibility: hidden; } #start_chat_but { visibility: hidden; } #fpix_wrapper { visibility: visible; } body { } &lt;/style&gt; &lt;style&gt; span {display:text !important;] &lt;/style&gt;&nbsp;
+<p><p>
+<b>Change Tab Name:</b>
+<br>
+&lt;script language=javascript&gt;document.title=(&ldquo;ENTER TITLE HERE&rdquo;)&lt;/script&gt; &nbsp;
+<p>
+<b>Place Weebly:</b>
+<br>
+&lt;iframe width=&ldquo;100%&rdquo; height=&ldquo;100%&rdquo; frameborder=&ldquo;0&rdquo; title=&ldquo;none&ldquo; src=&ldquo;<strong>YOUR WEEBLY CODE</strong>&rdquo;&gt;&lt;/iframe&gt;&nbsp;
+<br>
+<i>example of a weebly code; http://nameofyoursite.weebly.com/</i>
+</blockquote>
+
+
+
+
+
+
+
+<h1>Helpful Tools</h1>
+<blockquote>
+
+<li>
+<a href="http://tinypic.com/">TinyPic</a>
+/ <a href="http://upl.co/">Upl.co</a>
+/ <a href="https://streamable.com/">Streamable</a>
+(Image/Video Hosting)
+</li>
+
+<li>
+<a href="http://scratchpad.io/">Scratchpad.io</a>
+(Live CSS Preview)
+</li>
+
+<li>
+<a href="https://pixlr.com/editor/">Pixlr</a> (Photoshop Alternative)
+</li>
+
+<li>
+<a href="http://whatismyscreenresolution.net/multi-screen-test">Screentest</a> (Resolution Testing)
+</li>
+
+<li>
+<a href="http://kiwi6.com/">Kiwi6</a>&nbsp;(MP3 Hosting)
+</li>
+
+<li>
+<a href="https://ytmp3.cc/">YTMP3</a> (MP3 Converting)
+</li>
+
+<li>
+<a href="https://color.adobe.com/">Adobe Hex Color Wheel</a>
+</li>
+
+<li>
+<a href="http://fsymbols.com/">Symbols, Emoticons, etc.</a>
+</li>
+
+<li>
+<a href="http://www.upl.co/uploads/uspostalcodes01502824003.zip">All American Zip Codes</a> (Excel File)
+</li>
+</blockquote>
+
+
+
+
+
+
+<br><br>
+<!--content end-->
+    </div>
+</div>
+
+ 
+   
+</body>
+</html>
+
 
 
 '; ?>
