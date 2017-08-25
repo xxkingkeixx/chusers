5f733a2dcc9bea591e432bf9c4c44293dca3218b
solicited <mitchanimugen@gmail.com>
2017-08-25 01:48:15 +0000

    added tutorials

--- tutorials/sheepplayer.php
+++ tutorials/sheepplayer.php
@@ -1,1 +1,177 @@
-sh player test
+
+<!DOCTYPE html>
+<head>
+ 
+    <title>Tutorials</title>
+    <link rel="shortcut icon" href="https://1001freedownloads.s3.amazonaws.com/vector/thumb/126060/Anonymous_aiga_information_.png">
+    <link rel="alternate" type="application/rss+xml" href="{RSS}">
+   
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+<script src="http://static.tumblr.com/iuw14ew/VSQma1786/jquery.style-my-tooltips.js"></script>
+ 
+<script>
+ 
+(function($){
+ 
+$(document).ready(function(){
+ 
+$("a[title]").style_my_tooltips({
+ 
+tip_follows_cursor:true,
+ 
+tip_delay_time:100,
+ 
+tip_fade_speed:300,
+ 
+attribute:"title"
+ 
+});
+ 
+});
+ 
+})(jQuery);
+ 
+</script>
+
+<script>
+$(document).ready(function(){
+  $(".what").click(function(){
+        $('.img').toggleClass('top');
+        $(this).toggleClass('rotate')
+  });
+});
+</script>
+ 
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,400' rel='stylesheet' type='text/css'>
+ 
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+   
+    <style type="text/css">
+ 
+#s-m-t-tooltip {
+    max-width:150px;
+    padding:4px 6px;
+    margin:20px 14px 7px 10px;
+    background-color:#fff;
+    font-family:Calibri;
+    font-size:10px;
+    line-height:11px;
+    letter-spacing:1px;
+    border:1px solid #aaa;
+    text-transform:uppercase;
+    color:#777;
+    z-index:99999999;
+}
+   
+::-webkit-scrollbar {
+    height:0px;
+    width:3px;
+    background-color: transparent;
+}
+   
+::-webkit-scrollbar-thumb {
+    background-color: #555; /*scrollbar color*/
+}
+ 
+::selection {
+    background-color: #000;
+    color: #fff;
+}
+   
+body {
+    background-color: #eee; /*page background color*/
+    font-family:arial;
+    font-size:12px;
+}
+     
+a {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color: #bbb; /*link color*/
+}
+   
+a:hover {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color:#666; /*link hover color*/
+}
+
+#con {
+    width:750px;
+    height:520px;
+    overflow:hidden;
+    margin:auto;
+    position:fixed;
+    background:#fff; /*content background*/
+    left:0;
+    border-radius:3px;
+    right:0;
+    top:0;
+    bottom:0;
+}
+
+
+.content {
+    height:450px;
+    padding:0px 50px 50px;
+    overflow:auto;
+    color:#777; /*content color*/
+    line-height:170%;
+}
+
+.content p {
+    margin:25px 0px;
+}
+
+.content p:first-of-type {
+    margin-top:5px;
+}
+
+h1{
+background:#transparent;
+text-transform:uppercase;
+margin-bottom:2px;
+border-bottom:1px dotted #000000;
+color:#000000;
+font-size:9px;
+font-family: 'Verdana', serif;
+}
+
+h2{
+background:#transparent;
+text-transform:uppercase;
+margin-bottom:2px;
+border-bottom:0px dotted #000000;
+color:#000000;
+font-size:9px;
+font-family: 'Verdana', serif;
+}
+
+</style>
+</head>
+<body>
+
+<div id="con">
+    <div class="content">
+<!--content begin-->
+<br><br>
+
+<p><h1>Music Player Alternative #3: Billy Sheep Player</h1></p>
+
+
+<p><b>1. Go to the Billy Player website <a href="http://www.sheepproductions.com/billy/">here</a>.</b></p><p><b>2. You’ll need to use a direct url for the player. You can get such urls by uploading songs to <a href="http://kiwi6.com/">Kiwi6</a> or <a href="https://www.dropbox.com/">Dropbox</a>. The tutorial for that is <a href="http://hngthemes.tumblr.com/post/147913765213/how-to-convert-a-youtube-video-into-an-mp3-file">here</a>.</b> </p><p><i>For this tutorial I’ll use the Kiwi6 url as well as the Dropbox url.</i></p><figure data-orig-width="573" data-orig-height="566" class="tmblr-full"><img src="https://68.media.tumblr.com/f55155e427e81147e6029eabf3a8065b/tumblr_inline_oauhpccB1m1rzyom4_540.png" alt="image" data-orig-width="573" data-orig-height="566"></figure><figure data-orig-width="541" data-orig-height="345" class="tmblr-full"><img src="https://68.media.tumblr.com/aaaf0fa24ae93b084f112e1eed9c119d/tumblr_inline_oauhopbwmz1rzyom4_540.png" alt="image" data-orig-width="541" data-orig-height="345"></figure><p><b>3. The generator will then give you your code. The code should then be put under the section within your profile (if it is made by me) called &lt;!--- MUSIC PLAYER ---&gt;. If you cannot find this section, try Ctrl+F and manually enter in the name of the section.</b></p>
+
+
+<!--content end-->
+    </div>
+</div>
+
+ 
+   
+</body>
+</html>
