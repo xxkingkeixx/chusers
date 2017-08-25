e26a44c7baa5c873d82d98d63eaad4e3db307a96
solicited <mitchanimugen@gmail.com>
2017-08-25 22:23:34 +0000

    ok

--- index.php
+++ index.php
@@ -31,7 +31,8 @@
 									</header>
 								</div>
 								<div class="4u 12u$(medium)">
-									<p>Below you will be able to search for alphabetically for a username that fits you. Because of the ridiculous amount of usernames available, the prices of these usernames will be vastly lower than original market prices. This will put an end to the elitists selling accounts for $300 or more.</p>
+									<p>You will be able to search alphabetically for a username that fits you. Because of the immense amount of usernames available, the prices of these usernames will be vastly lower than their usual market prices. This will put an end to the elitists selling accounts for $300 or more.</p>
+									
 									
 									<center><a href="/explore" class="button">Explore Available Accounts</a></center>
 		
