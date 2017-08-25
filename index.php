88a78469f970d684d59bb186ed0029bed2b54b4e
solicited <mitchanimugen@gmail.com>
2017-08-25 23:13:26 +0000

    ok

--- index.php
+++ index.php
@@ -65,7 +65,9 @@
 						<p>
 							Accounts are secured for a smooth transition to your ownership. All payments will be made through Paypal so that your details are protected. 
 							<br><br>
-							For your own safety, please do NOT disclose the username you are purchasing to ANYONE before registering it under your own email. Failure to do so may lead to someone you know registering your account. If you have any questions or concerns you can message the developers, or the helper bot (coming soon).
+							For your own safety, please do NOT disclose the username you are purchasing to ANYONE before registering it under your own email. Failure to do so may lead to someone you know registering your account. 
+							<br><br>
+							If you have any questions or concerns you can message the developers, or the helper bot (coming soon).
 						</p>
 					</div>
 					<a href="#three" class="goto-next scrolly">Next</a>
