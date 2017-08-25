b6d475d428f554466c14be7356b84333534aaa49
solicited <mitchanimugen@gmail.com>
2017-08-25 22:15:46 +0000

    ok

--- explore.php
+++ explore.php
@@ -21,21 +21,40 @@
     <link href="assets/css/explorecss/sidebar-menu.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/explorecss/animate.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/explorecss/jquery-ui.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-    <audio id="beep" preload="auto">
-	
-    <!--     Change Audio Here  -->   	
-    <source src="audio/click.ogg"></source>
-    Your browser isn"t invited for super fun audio time.
-    </audio>
-    </head>
+</head>
 
 <body>
-    <?php require 'models/navbar.php' ?>
+    	<header id="header">
+					<h1 id="logo"><a href="http://chatangu.tk/">Chatangu</a></h1>
+					<nav id="nav" style="margin-right:40px;">
+						<ul>
+							<li id="list"><a href="/" class="fa fa-home fa-fw"></a></li>
+							<li id="list"> <form method="post" action="search_results.php"><input id="search" type="text" style ="border-radius:33px;height:25px;" method="post" name="user_name" maxlength="50" " class="form-control" placeholder="Search for Usernames" /></form></li>
+						<!-- Developers -->
+							<li id="list">
+								<a href="/">Developers</a>
+							</li>
+						<!--Tutorials -->
+							<li id="list">
+								<a href="tutorials/tutorials">Tutorials</a>
+							</li>
+						<!-- Bot -->	
+							<li id="list">
+								<a href="#">Bot</a>
+							<ul>
+								<li><a href="#">Info</a>
+								<ul>
+							<li><a href="#"> Developer</a></li>
+							<li><a href="#">Documentation</a></li>
+							<li><a href="#">Progress</a></li>
+							<li><a href="#">Suggestions</a></li>
+							</ul>
+							</li>
+							<li><a href="#">Report Bugs</a></li></ul>
+							</ul>
+							</li>
+					</nav>
+				</header>
 
     <!-- Preloader -->
     <section id="preloader">
