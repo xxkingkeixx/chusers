848df7d2bc9728838909e224a7c4aa3aa4f06843
solicited <mitchanimugen@gmail.com>
2017-08-25 01:50:39 +0000

    added tutorials

--- tutorials/miniflashplayer.php
+++ tutorials/miniflashplayer.php
@@ -1,1 +1,177 @@
-mini test
+
+<!DOCTYPE html>
+<head>
+ 
+    <title>Tutorials</title>
+    <link rel="shortcut icon" href="https://1001freedownloads.s3.amazonaws.com/vector/thumb/126060/Anonymous_aiga_information_.png">
+    <link rel="alternate" type="application/rss+xml" href="{RSS}">
+   
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+<script src="http://static.tumblr.com/iuw14ew/VSQma1786/jquery.style-my-tooltips.js"></script>
+ 
+<script>
+ 
+(function($){
+ 
+$(document).ready(function(){
+ 
+$("a[title]").style_my_tooltips({
+ 
+tip_follows_cursor:true,
+ 
+tip_delay_time:100,
+ 
+tip_fade_speed:300,
+ 
+attribute:"title"
+ 
+});
+ 
+});
+ 
+})(jQuery);
+ 
+</script>
+
+<script>
+$(document).ready(function(){
+  $(".what").click(function(){
+        $('.img').toggleClass('top');
+        $(this).toggleClass('rotate')
+  });
+});
+</script>
+ 
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,400' rel='stylesheet' type='text/css'>
+ 
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+   
+    <style type="text/css">
+ 
+#s-m-t-tooltip {
+    max-width:150px;
+    padding:4px 6px;
+    margin:20px 14px 7px 10px;
+    background-color:#fff;
+    font-family:Calibri;
+    font-size:10px;
+    line-height:11px;
+    letter-spacing:1px;
+    border:1px solid #aaa;
+    text-transform:uppercase;
+    color:#777;
+    z-index:99999999;
+}
+   
+::-webkit-scrollbar {
+    height:0px;
+    width:3px;
+    background-color: transparent;
+}
+   
+::-webkit-scrollbar-thumb {
+    background-color: #555; /*scrollbar color*/
+}
+ 
+::selection {
+    background-color: #000;
+    color: #fff;
+}
+   
+body {
+    background-color: #eee; /*page background color*/
+    font-family:arial;
+    font-size:12px;
+}
+     
+a {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color: #bbb; /*link color*/
+}
+   
+a:hover {
+    text-decoration: none;
+    -webkit-transition: 0.5s;
+    -moz-transition: 0.5s;
+    transition: 0.5s;
+    color:#666; /*link hover color*/
+}
+
+#con {
+    width:750px;
+    height:520px;
+    overflow:hidden;
+    margin:auto;
+    position:fixed;
+    background:#fff; /*content background*/
+    left:0;
+    border-radius:3px;
+    right:0;
+    top:0;
+    bottom:0;
+}
+
+
+.content {
+    height:450px;
+    padding:0px 50px 50px;
+    overflow:auto;
+    color:#777; /*content color*/
+    line-height:170%;
+}
+
+.content p {
+    margin:25px 0px;
+}
+
+.content p:first-of-type {
+    margin-top:5px;
+}
+
+h1{
+background:#transparent;
+text-transform:uppercase;
+margin-bottom:2px;
+border-bottom:1px dotted #000000;
+color:#000000;
+font-size:9px;
+font-family: 'Verdana', serif;
+}
+
+h2{
+background:#transparent;
+text-transform:uppercase;
+margin-bottom:2px;
+border-bottom:0px dotted #000000;
+color:#000000;
+font-size:9px;
+font-family: 'Verdana', serif;
+}
+
+</style>
+</head>
+<body>
+
+<div id="con">
+    <div class="content">
+<!--content begin-->
+<br><br>
+
+<p><h1>Music Player Alternative #1: Mini Flash Player</h1></p>
+
+
+<b>1. Go to the <a href="http://www.strangecube.com/audioplay/">Audioplay Generator</a>. </b></p><p><b>2. You’ll need to use a direct url for the player. You can get such urls by uploading songs to <a href="http://kiwi6.com/">Kiwi6</a> or <a href="https://www.dropbox.com/">Dropbox</a>. The tutorial for that is <a href="http://hngthemes.tumblr.com/post/147913765213/how-to-convert-a-youtube-video-into-an-mp3-file">here</a>.</b> <br></p><p><i>I recommend using Kiwi6 for this player because some Dropbox links won’t work.</i></p><figure data-orig-width="733" data-orig-height="570" class="tmblr-full"><img src="https://68.media.tumblr.com/4e87ece1a1044e6f09e4ff2fd9d609af/tumblr_inline_oauhdrsQcN1rzyom4_540.png" alt="image" data-orig-width="733" data-orig-height="570"></figure><p><b>3. The generator will then give you your code on the right hand side. The code should then be put under the section within your profile (if it is made by me) called &lt;!--- MUSIC PLAYER ---&gt;. If you cannot find this section, try Ctrl+F and manually enter in the name of the section.</b></p><p><p><i>**Although I wouldn’t recommend it, it is possible to move the player to any position across your profile. Please message me for further information on that, because it requires extra coding.</i></p><p><i><b>**THIS PLAYER CANNOT AUTOPLAY IN CHROME. Possible bug with the formatting compatibility with chrome.</b></i></p>
+
+
+<!--content end-->
+    </div>
+</div>
+
+ 
+   
+</body>
+</html>
