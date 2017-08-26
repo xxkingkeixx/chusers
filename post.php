6ad34c60ac4dde952efc24cc39af8dcaa94f105a
solicited <mitchanimugen@gmail.com>
2017-08-26 21:54:04 +0000

    ok

--- post.php
+++ post.php
@@ -93,6 +93,14 @@ mysqli_query($conn, "UPDATE store SET class = 'offer' , price= '1' WHERE  user_n
 
 
 
+if(isset($_POST['bought']))
+{
+mysqli_query($conn, "UPDATE store SET buyer = '$_POST[buyer]' WHERE  user_name = '$_POST[bought]'");
+}
+
+
+
+
 
     
             
