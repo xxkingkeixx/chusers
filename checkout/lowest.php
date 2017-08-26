b2a8fe74bf35cbde751549f8f1c2ba5bc094bb54
solicited <mitchanimugen@gmail.com>
2017-08-26 20:45:22 +0000

    ok

--- checkout/lowest.php
+++ checkout/lowest.php
@@ -1,4 +1,17 @@
 
+
+<?php require $_SERVER["DOCUMENT_ROOT"] . "/models/header.php"; ?>
+<?php require $_SERVER["DOCUMENT_ROOT"] . "/models/navbar.php"; ?>
+
+
+	<!-- Banner -->
+				<section id="banner">
+					<div class="content">
+						<header>
+
+<h1> Purchasing a Legendary</h1>
+
+
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="YJU9LC27X5SG4">
@@ -10,6 +23,16 @@
 <tr><td><input type="hidden" name="on2" value="What username are you buying?">What username are you buying?</td></tr><tr><td><input type="text" name="os2" maxlength="200"></td></tr>
 </table>
 <input type="hidden" name="currency_code" value="USD">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<input type="submit" class='special' border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+
+
+	</header>
+						<span class="image"><img src="../images/pic01.jpg" alt="" /></span>
+					</div>
+						
+				</section>
+
+<?php require $_SERVER["DOCUMENT_ROOT"] . "/models/footer.php"; ?>
+
